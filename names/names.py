93ead3b90cf5e8e48ef63cb527274f2bc811a28b
lndubose <lndubose13@gmail.com>
2019-01-25 15:07:58 -0600

    Contains was wrong. Fix now

--- names/names.py
+++ names/names.py
@@ -84,3 +84,68 @@ for name in names_2_set:
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
+
+"""
+# Use my way, contains is now O(log n)
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        node = BinarySearchTree(value)
+
+        def rec(target, current_node):
+            if target > current_node.value:
+                if current_node.right is None:
+                    current_node.right = node
+                    return
+                else:
+                    rec(target, current_node.right)
+            elif target < current_node.value:
+                if current_node.left is None:
+                    current_node.left = node
+                    return
+                else:
+                    rec(target, current_node.left)
+
+        return rec(value, self)
+
+    def contains(self, target):
+        def rec(value, current_node):
+            if current_node is None:
+                return False
+            elif value == current_node.value:
+                return True
+            elif value > current_node.value:
+                return rec(value, current_node.right)
+            elif value < current_node.value:
+                return rec(value, current_node.left)
+
+        return rec(target, self)
+
+    def get_max(self):
+        maxValue = -float("inf")
+        current_node = self
+        while current_node is not None:
+            if current_node.value > maxValue:
+                maxValue = current_node.value
+            current_node = current_node.right
+
+        return maxValue
+
+
+duplicates = []
+
+firstName = names_2[0]
+
+bst = BinarySearchTree(firstName)
+
+for name in names_2:
+    bst.insert(name)
+
+for name in names_1:
+    if bst.contains(name):
+        duplicates.append(name)
+"""
