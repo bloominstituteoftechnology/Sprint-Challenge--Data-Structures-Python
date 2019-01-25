f4b535a7b5219b4d085de16713484306932c06f5
stdemicheli <stdemicheli@gmail.com>
2019-01-25 19:11:48 +0100

    Implements names stretch.

--- names/names.py
+++ names/names.py
@@ -27,3 +27,38 @@ end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+# Stretch:
+
+
+duplicates_stretch = []
+
+def quick_sort( arr ):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        low = [item for item in arr[1:] if item < pivot]
+        high = [item for item in arr[1:] if item > pivot]
+        return quick_sort(low) + [pivot] + quick_sort(high)
+
+    return arr
+
+
+i = 0
+j = 0
+sorted_1 = quick_sort(names_1)
+sorted_2 = quick_sort(names_2)
+
+while i < len(sorted_1) and j < len(sorted_2):
+  if sorted_1[i] == sorted_2[j]:
+    duplicates_stretch.append(sorted_1[i])
+    i += 1
+    j += 1
+  elif sorted_1[i] < sorted_2[j]:
+    i += 1
+  else:
+    j += 1
+
+end_time_stretch = time.time()
+print (f"{len(duplicates_stretch)} duplicates:\n\n{', '.join(duplicates_stretch)}\n\n")
+print (f"runtime: {end_time_stretch - end_time} seconds")
