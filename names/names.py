21e8162f76bfc994b6d383301987263584355f6f
Daniel Ferrer <danfrrr13@gmail.com>
2019-01-25 13:11:46 -0500

    Optimized names.py to just over 1 second.

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BinarySearchTree 
 
 start_time = time.time()
 
@@ -10,11 +11,16 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# I beleive the following is O(n) runtime solution. went from 6 seconds to 1 second.
+
+duplicates = [name for name in names_1 if name in names_2]
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
