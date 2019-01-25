bb4bc617dab4c76326597377ef4d4bdb92de5da7
Max <maxkajiwara@gmail.com>
2019-01-25 10:54:32 -0800

    Optimize names.py

--- names/names.py
+++ names/names.py
@@ -11,12 +11,25 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# Optimized using dictionary (hash table)
+name_dict = {}
+
+for name in names_1:
+    name_dict[name] = 0
+
+for name in names_2:
+    if name in name_dict:
+        name_dict[name] += 1
+
+for name in name_dict:
+    if name_dict[name]:
+        duplicates.append(name)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
