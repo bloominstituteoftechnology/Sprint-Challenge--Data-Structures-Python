b377cebeff17798e5c3bb29b6426e936e2ba320f
Ryan Walker <rytwalker@gmail.com>
2019-01-25 11:16:43 -0500

    Init commit

--- search/test_breadth_first_search.py
+++ search/test_breadth_first_search.py
@@ -1,31 +1,34 @@
 import unittest
 from binary_search_tree import BinarySearchTree
 
+
 class BinarySearchTreeTests(unittest.TestCase):
-  def setUp(self):
-    self.bst = BinarySearchTree(5)
+    def setUp(self):
+        self.bst = BinarySearchTree(5)
 
-  def test_breadth_first_traversal_1(self):
-    arr = []
-    cb = lambda x: arr.append(x)
+    def test_breadth_first_traversal_1(self):
+        arr = []
 
-    self.bst.breadth_first_for_each(cb)
+        def cb(x): return arr.append(x)
 
-    self.assertEqual(arr, [5])
+        self.bst.breadth_first_for_each(cb)
 
-  def test_breadth_first_traversal_2(self):
-    arr = []
-    cb = lambda x: arr.append(x)
+        self.assertEqual(arr, [5])
 
-    self.bst.insert(3)
-    self.bst.insert(4)
-    self.bst.insert(10)
-    self.bst.insert(9)
-    self.bst.insert(11)
-    self.bst.breadth_first_for_each(cb)
+    def test_breadth_first_traversal_2(self):
+        arr = []
 
-    self.assertEqual(arr, [5, 3, 10, 4, 9, 11])
+        def cb(x): return arr.append(x)
+
+        self.bst.insert(3)
+        self.bst.insert(4)
+        self.bst.insert(10)
+        self.bst.insert(9)
+        self.bst.insert(11)
+        self.bst.breadth_first_for_each(cb)
+
+        self.assertEqual(arr, [5, 3, 10, 4, 9, 11])
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
