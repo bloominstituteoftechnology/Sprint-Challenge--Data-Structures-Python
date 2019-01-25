816510f01464b49b06fa4a85826acfc40f3e2242
Andrew <ghobrial.andrew@gmail.com>
2019-01-25 13:15:12 -0500

    names.py

--- names/names.py
+++ names/names.py
@@ -10,11 +10,23 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# create a dictionary of name_1 to compare against
+name_1_dictionary = {}
+
+# duplicates array to aggregate matches
 duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    name_1_dictionary[name_1] = True
+
+# if name_2 is found in name_1 dictionary using get() method, add it to duplicates
+for name_2 in names_2:
+    if name_1_dictionary.get(name_2, None):
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
