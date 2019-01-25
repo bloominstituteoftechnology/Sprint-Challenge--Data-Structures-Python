67124189de6c124c7ef1c57f1b7f63dd3af59443
Daniel Ferrer <danfrrr13@gmail.com>
2019-01-25 15:45:07 -0500

    Optimized names.py further by using a set for the comparison.

--- names/names.py
+++ names/names.py
@@ -10,15 +10,17 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-# duplicates = [] # O(n) space complexity
-# O(n**2) time complexity
-# for name_1 in names_1:  # O(n)
-#     for name_2 in names_2: # O(n)
-#         if name_1 == name_2: # O(1)
-#             duplicates.append(name_1) # O(1)
+duplicates = [] 
+set_names = set()
+set_names.add(tuple(names_1))
+for name in names_2:
+    if name in set_names:
+        duplicates.append(name)
+    
 
 # I beleive the following is still a O(n^2) runtime solution. But the time went from 6 seconds to 1 second.
-duplicates = [name for name in names_1 if name in names_2]
+# duplicates = [name for name in names_1 if name in names_2]
+
 
 
 end_time = time.time()
