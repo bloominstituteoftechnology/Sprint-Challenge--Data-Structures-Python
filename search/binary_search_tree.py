6a85690d91cc1a3b3b7a6b2f9b5080f456423625
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-01-25 12:24:05 -0500

    DFS

--- search/binary_search_tree.py
+++ search/binary_search_tree.py
@@ -5,8 +5,12 @@ class BinarySearchTree:
         self.right = None
 
     def depth_first_for_each(self, cb):
-        visited = []
-        while self.value
+        cb(self.value)
+        if self.left:
+            self.left.depth_first_for_each(cb)
+        if self.right:
+            self.right.depth_first_for_each(cb)
+        return
 
     def breadth_first_for_each(self, cb):
         pass
