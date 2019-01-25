14ea6f13713c1691d12f6ad2e07591276a74bd65
Daniel Ferrer <danfrrr13@gmail.com>
2019-01-25 12:45:17 -0500

    Been working on breadth_first_search method this whole time. Did not reach out to PM.

--- search/binary_search_tree.py
+++ search/binary_search_tree.py
@@ -8,7 +8,27 @@ class BinarySearchTree:
     pass    
 
   def breadth_first_for_each(self, cb):
-    pass
+    visited = []
+    queue = []
+    if self.value:
+      queue.append(self.value)
+    while queue:
+      print('queue = ', queue)
+      print('visited = ', visited)
+      if queue[0]
+      vertex = queue.pop(0)
+      if vertex not in visited:
+        # if vertex.value:
+        #   visited.append(vertex.value)
+        # else:
+        visited.append(vertex)
+        if self.left:
+          queue.append(self.left)
+        if self.right:
+          queue.append(self.right)
+    for i in visited:
+      return cb(i)
+
 
   def insert(self, value):
     new_tree = BinarySearchTree(value)
@@ -44,3 +64,28 @@ class BinarySearchTree:
         max_value = current.value
       current = current.right
     return max_value
+
+
+bst = BinarySearchTree(5)
+bst.insert(3)
+bst.insert(4)
+bst.insert(10)
+bst.insert(9)
+bst.insert(11)
+
+print(bst.contains(5))
+print(bst.contains(3))
+print(bst.contains(4))
+
+arr = []
+cb = lambda x: arr.append(x)
+
+bst.breadth_first_for_each(cb)
+print(arr)
+
+# bst.insert(3)
+# bst.insert(4)
+# bst.insert(10)
+# bst.insert(9)
+# bst.insert(11)
+
