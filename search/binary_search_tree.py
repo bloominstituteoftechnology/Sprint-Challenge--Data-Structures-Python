e260cb199f2772c018b3b068de34af77a6e88774
lndubose <lndubose13@gmail.com>
2019-01-25 10:15:03 -0600

    Add Queue Class

--- search/binary_search_tree.py
+++ search/binary_search_tree.py
@@ -1,46 +1,74 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def depth_first_for_each(self, cb):
-    pass    
+    def depth_first_for_each(self, cb):
+        pass
 
-  def breadth_first_for_each(self, cb):
-    pass
+    def breadth_first_for_each(self, cb):
+        pass
 
-  def insert(self, value):
-    new_tree = BinarySearchTree(value)
-    if (value < self.value):
-      if not self.left:
-        self.left = new_tree
-      else:
-        self.left.insert(value)
-    elif value >= self.value:
-      if not self.right:
-        self.right = new_tree
-      else:
-        self.right.insert(value)
+    def insert(self, value):
+        new_tree = BinarySearchTree(value)
+        if (value < self.value):
+            if not self.left:
+                self.left = new_tree
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if not self.right:
+                self.right = new_tree
+            else:
+                self.right.insert(value)
 
-  def contains(self, target):
-    if self.value == target:
-      return True
-    if self.left:
-      if self.left.contains(target):
-        return True
-    if self.right:
-      if self.right.contains(target):
-        return True
-    return False
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if self.left:
+            if self.left.contains(target):
+                return True
+        if self.right:
+            if self.right.contains(target):
+                return True
+        return False
 
-  def get_max(self):
-    if not self:
-      return None
-    max_value = self.value
-    current = self
-    while current:
-      if current.value > max_value:
-        max_value = current.value
-      current = current.right
-    return max_value
+    def get_max(self):
+        if not self:
+            return None
+        max_value = self.value
+        current = self
+        while current:
+            if current.value > max_value:
+                max_value = current.value
+            current = current.right
+        return max_value
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
+
+    def enqueue(self, item):
+        self.storage.append(item)
+        self.size += 1
+
+    def dequeue(self):
+        if self.storage is None:
+            return
+        self.size += -1
+        return self.storage.pop(0)
+
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.dequeue()
+q.dequeue()
+print(q.dequeue())
+
+
+print(q.storage)
