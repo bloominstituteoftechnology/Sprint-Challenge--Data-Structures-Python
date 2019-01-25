3508c8a3a0441c6c6fadab7aa6636e5940d16c54
ezra davis davis <ezra.rr.davis@gmail.com>
2019-01-25 11:02:57 -0800

    O(n) names finished

---
+++ names/names2.py
@@ -0,0 +1,26 @@
+import time
+
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates = []
+dictionary = {}
+for name_1 in names_1:
+  dictionary[name_1] = name_1
+for name_2 in names_2:
+  try:
+    duplicates.append(dictionary[name_2])
+  except KeyError:
+    pass
+    
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
