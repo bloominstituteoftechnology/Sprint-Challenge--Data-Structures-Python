b84e6a112d362d4a75d5248c7b0c1913057fdaea
ckirkwood94 <ckirkwood94@gmail.com>
2019-01-25 13:30:59 -0500

    Names.py complete

--- names/names.py
+++ names/names.py
@@ -10,13 +10,21 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# Create dictionary to check names against
+check = {}
+# create list to store duplicates
 duplicates = []
+# Loop through first list and add to check
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    check[name_1] = True
+
+# Loop through second list and get name out of check dict
+# If in check add to duplicates
+for name_2 in names_2:
+    if check.get(name_2, None):
+        duplicates.append(name_2)
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
