a7ed014110d9ea1d0d2cc2779f1ee4245395961c
Kevin <brackkevin@gmail.com>
2019-01-25 12:18:06 -0600

    Imports binary_search_tree.py file I created during the week

---
+++ names/binary_search_tree.py
@@ -0,0 +1,37 @@
+class BinarySearchTree:
+  def __init__(self, value):
+    self.value = value
+    self.left = None
+    self.right = None
+
+  def insert(self, value):
+    if value < self.value:
+      if self.left == None:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+    else:
+      if self.right == None:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
+
+  def contains(self, target):
+    if self.value == target:
+      return True
+    elif target < self.value:
+      if self.left is not None:
+        return self.left.contains(target)
+      else:
+        return False
+    elif target > self.value:
+      if self.right is not None:
+        return self.right.contains(target)
+      else:
+        return False
+
+  def get_max(self):
+    if self.right is not None:
+      return self.right.get_max()
+    else:
+      return self.value
