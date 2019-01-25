a0e7e06736f6b19ba17e8502879f3b9a0c2f5fdb
michaelagard <mdagard@gmail.com>
2019-01-25 15:49:40 -0500

    after review, figured out names

--- names/names.py
+++ names/names.py
@@ -10,13 +10,15 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+names_1_dict = {}
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+for name in names_1:
+    names_1_dict[name] = None
+for name in names_2:
+    if name in names_1_dict:
+        duplicates.append(name)
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
