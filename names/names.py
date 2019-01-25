a8f7e6686ffd907be7f325d6062dd5ec60d66dde
Daniel Ferrer <danfrrr13@gmail.com>
2019-01-25 15:49:25 -0500

    Fixed the set in names.py

--- names/names.py
+++ names/names.py
@@ -11,13 +11,13 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = [] 
-set_names = set()
-set_names.add(tuple(names_1))
+set_names = set(names_1)
 for name in names_2:
     if name in set_names:
         duplicates.append(name)
-    
 
+print(set_names)
+    
 # I beleive the following is still a O(n^2) runtime solution. But the time went from 6 seconds to 1 second.
 # duplicates = [name for name in names_1 if name in names_2]
 
