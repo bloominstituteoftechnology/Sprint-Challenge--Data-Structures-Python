3d3c0f09324a77a21c2150ffd94eb1228368802b
luiscmartinez <mrluismartinezzz@gmail.com>
2019-01-25 10:24:13 -0800

    implemented depth_first_for_each method

--- search/binary_search_tree.py
+++ search/binary_search_tree.py
@@ -5,7 +5,11 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-        
+    cb(self.value)
+    if self.left is not None:
+      self.left.depth_first_for_each(cb)
+    if self.right is not None:
+      self.right.depth_first_for_each(cb)    
 
   def breadth_first_for_each(self, cb):
     pass
