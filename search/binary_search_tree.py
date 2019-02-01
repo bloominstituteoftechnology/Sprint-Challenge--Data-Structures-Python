4b3eed819601adb2ebb927407a7384304711aa9a
sumi419 <sasgar04@gmail.com>
2019-02-01 08:59:24 -0800

    DFS preorder

--- search/binary_search_tree.py
+++ search/binary_search_tree.py
@@ -10,7 +10,12 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-    pass    
+    # preorder
+    cb(self.value)
+    if self.left:
+      self.left.depth_first_for_each(cb)
+    if self.right:
+      self.right.depth_first_for_each(cb)
 
   def breadth_first_for_each(self, cb):
     pass
