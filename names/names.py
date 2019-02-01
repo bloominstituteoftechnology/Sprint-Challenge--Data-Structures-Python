577695b08072ab9d46afd6fa445018f6c6b664dd
sumi419 <sasgar04@gmail.com>
2019-02-01 10:20:55 -0800

    names refactored

--- names/names.py
+++ names/names.py
@@ -11,11 +11,16 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:  #O(n)
+#     for name_2 in names_2:   #O(n)
+#         if name_1 == name_2:   
+#             duplicates.append(name_1)  #O(1)
 
+set_names_1 = set(names_1) 
+set_names_2 = set(names_2)
+set_names = set_names_1|set_names_2 #make union between sets
+duplicate_names = set_names_1 & set_names_2 #intersetion prints out only repeated names
+duplicates = list(duplicate_names)
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
