01599b3b25522dcb23e22a3cc3f90cf931379bb9
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-02-10 16:22:52 -0500

    added pseudocode for DFS solution

--- search/binary_search_tree.py
+++ search/binary_search_tree.py
@@ -5,7 +5,14 @@ class BinarySearchTree:
     self.right = None
 
   def depth_first_for_each(self, cb):
-    pass    
+    """
+    call the anonymous function on each node in the tree 
+    this will require a current node variable to reference cb(current.value) for nodes
+    for depth-first search we'll traverse the left tree as long as possible
+    if this is not possible, go to the previous node
+    from previous node, go down the right path until no longer possible, then the left path
+    when these paths are complete, go back to previous node again and repeat
+    """    
 
   def breadth_first_for_each(self, cb):
     pass
