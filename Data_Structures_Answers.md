ad942ca7d3992a412c045f737bbd3e8e9fb69761
tryingtokeepup <kl.hawaii@gmail.com>
2019-04-05 08:58:33 -0700

    is this updating?

--- Data_Structures_Answers.md
+++ Data_Structures_Answers.md
@@ -1,13 +1,17 @@
 Add your answers to the questions below.
 
 1. What is the runtime complexity of your ring buffer's `append` method?
-
+   Constant time, all i am doing is popping or inserting to the top, without making the list all over again.
 2. What is the space complexity of your ring buffer's `append` function?
-
+   Pretty sure this is constant too, as we are not making extra arrays but are in place.
 3. What is the runtime complexity of your ring buffer's `get` method?
-
+   <!-- Constant time, we always have as many items to call as the capacity allows,  -->
+   Actually, come to think of it, this is probably linear time, as we pass in the capacity at the beginning of the argument.
+   It doesn't get larger than the capacity, so I am not sure how to answer this, but if we take into consideration
+   that the get() method will always be constant with the capacity, and we need to list all items in the array, you can argue
+   this method takes linear time.
 4. What is the space complexity of your ring buffer's `get` method?
-
+   Constant space, all we are doing is calling the list with that capacity amount.
 
 5. What is the runtime complexity of the provided code in `names.py`?
 
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,44 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        if self.capacity > self.current:
+            self.storage.pop(0)
+            self.storage.append(item)
+            self.current += 1
 
-  def get(self):
-    pass
+        elif self.capacity == self.current:
+            self.storage.pop(0)
+            self.storage.insert(0, item)
+            self.current += 1
+        else:
+            self.storage.pop(self.capacity - 1)
+            self.storage.insert(0, item)
+
+    def get(self):
+        storage = self.storage
+        storage = [x for x in storage if x is not None]
+        return storage
+
+
+buf = RingBuffer(4)
+
+
+buf.append(1)
+print(buf.get())
+buf.append(1)
+print(buf.get())
+buf.append(3)
+print(buf.get())
+buf.append(6)
+print(buf.get())
+buf.append(10)
+print(buf.get())
+# buf.append(9)
+# print(buf.get())
+# buf.append(3)
+# buf.append(6)
+# print(buf.get())
