961708726d7a80766ccba2e2cc0e3d3c6ed525a2
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-04-05 11:27:40 -0500

    Names complete in 0.05 seconds

--- names/names.py
+++ names/names.py
@@ -11,12 +11,37 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+my_arr = []
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    my_arr.append(name_1)
+
+sorted_arr = sorted(my_arr)
+
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr)-1
+    while low <= high:
+        middle = int((low+high)/2)
+        midpoint = arr[middle]
+        if midpoint > target:
+            high = middle-1
+        elif midpoint < target:
+            low = middle+1
+        else:
+            return midpoint
+    return -1
+
+
+for name_2 in names_2:
+    if binary_search(sorted_arr, name_2) is not -1:
+        duplicates.append(name_2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
