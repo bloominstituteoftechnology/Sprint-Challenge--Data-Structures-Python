bc749d30dff46fc856d85e821962407668c97c37
Ben Hakes <bhhakes@gmail.com>
2019-05-17 11:35:50 -0500

    finish optimizing names.py for o(n) time complexity

--- names/names.py
+++ names/names.py
@@ -1,22 +1,32 @@
 import time
+import os
+from bst import BinarySearchTree
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open('names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open('names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+all_names = names_1 + names_2
+cache = dict()
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+for name in all_names:
+    try: 
+        if cache[name] == 1:
+            duplicates.append(name)
+    except KeyError as e:
+        cache[name] = 1
+        continue
+        
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+
 
