0e39788867e3e2d989e9d22472818e6dfb042796
Ben Hakes <bhhakes@gmail.com>
2019-05-17 12:53:17 -0500

    add names_stretch.py stretch goal implementation

--- names/names_stretch.py
+++ names/names_stretch.py
@@ -4,16 +4,19 @@ from bst import BinarySearchTree
 start_time = time.time()
 
 f = open('names/names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
+all_names = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 f = open('names/names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
+all_names += f.read().split("\n")  # List containing 10000 names
 f.close()
 
-all_names = names_1 + names_2
-
-duplicates = [name for name in names_1 if names_1.count(name) > 1]
+duplicates = []
+for name in all_names:
+    if all_names.count(name) > 1 and name not in duplicates:
+        duplicates.append(name)
+    else:
+        continue
 
 end_time = time.time()
 print (f"runtime: {end_time - start_time} seconds")
