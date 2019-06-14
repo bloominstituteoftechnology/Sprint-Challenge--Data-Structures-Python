8eaf1d5e5b2ae90afa374c50d3aa442f7e1867f5
AustinBCole <austin.cole.chileno@gmail.com>
2019-06-14 09:43:45 -0600

    names.py under 1/100 of a second.

--- names/names.py
+++ names/names.py
@@ -11,12 +11,17 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
+names_dict = {}
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    names_dict[name_1] = True
+for name_2 in names_2:
+    if name_2 in names_dict:
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+
 
