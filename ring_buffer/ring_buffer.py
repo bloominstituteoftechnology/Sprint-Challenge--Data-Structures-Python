d71939679806047dbdde789a20bb7811b68930bd
AustinBCole <austin.cole.chileno@gmail.com>
2019-06-14 09:28:40 -0600

    Finish with ring_buffer.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,16 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
-
+      # This storage is filled with None at the beginning. The first thing I need to do is check to see if the list contains None. If so, I replace the first instance of None with the item.
+    if None in self.storage:
+        first_instance = self.storage.index(None)
+        self.storage[first_instance] = item
+    else:
+        self.storage[self.current] = item
+        if self.current < self.capacity - 1:
+            self.current += 1
+        else:
+            self.current = 0
   def get(self):
-    pass
+    return_value = [i for i in self.storage if i != None]
+    return return_value
