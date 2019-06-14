6394482e3b36a8c2f99729b38f3aa9c13713f178
Thorben Bender <bender.thorben0@gmail.com>
2019-06-14 10:03:25 +0200

    names working

--- names/names.py
+++ names/names.py
@@ -10,13 +10,29 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+print(names_1)
+print(names_2)
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
+duplicates2 = []
+if len(names_1) > len(names_2):
+    for name_1 in names_1:
+        if name_1 in names_2:
             duplicates.append(name_1)
+else:
+    for name_2 in names_2:
+        if name_2 in names_1:
+            duplicates.append(name_2)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates2.append(name_1)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
+# duplicates.sort()
+# duplicates2.sort()
+
+# print(duplicates == duplicates2)
