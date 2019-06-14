daef42147a83c57ad2b9c437b5ada6115dbb6e1a
Carlos Gutierrez <chgutier@gmail.com>
2019-06-14 12:18:48 -0500

    Finished names and makdown with answers

--- Data_Structures_Answers.md
+++ Data_Structures_Answers.md
@@ -1,18 +1,18 @@
 Add your answers to the questions below.
 
 1. What is the runtime complexity of your ring buffer's `append` method?
-
+> O(1)
 2. What is the space complexity of your ring buffer's `append` function?
-
+> O(1)
 3. What is the runtime complexity of your ring buffer's `get` method?
-
+> O(k) --> __'k' is either the value of a parameter or the number of elements in the parameter__
 4. What is the space complexity of your ring buffer's `get` method?
-
-
+> O(1)
 5. What is the runtime complexity of the provided code in `names.py`?
-
+> O(n^2)
 6. What is the space complexity of the provided code in `names.py`?
-
+> O(n^2)
 7. What is the runtime complexity of your optimized code in `names.py`?
-
+> O(n)
 8. What is the space complexity of your optimized code in `names.py`?
+> O(1)
--- names/names.py
+++ names/names.py
@@ -1,6 +1,31 @@
-import time
+# import time
 
+# start_time = time.time()
+
+# f = open('names_1.txt', 'r')
+# names_1 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# f = open('names_2.txt', 'r')
+# names_2 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# end_time = time.time()
+# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print (f"runtime: {end_time - start_time} seconds")
+
+
+# _________ Carlos' version using "set" ________
+
+import time
 start_time = time.time()
+
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
@@ -10,13 +35,10 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# Finding intersection between the two sets and returnin list of duplicates
+duplicates = list(set(names_1) & set(names_2))
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
 
+print (f"{len(duplicates)} duplicates:\n\n{duplicates}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
