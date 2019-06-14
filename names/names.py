9aecca7f18124870089dc82f77409cc4faeb2bd6
Mark Oliver <mark.c.oliver@gmail.com>
2019-06-14 12:46:30 -0400

    partial stretch

--- names/names.py
+++ names/names.py
@@ -33,26 +33,35 @@ def search(lst, target):
     if max == -1:
         return None
     elif max == 0:
-        return min 
+        return max if lst[0] == target else None
     avg = (min+max) // 2
     # uncomment next line for traces
-    # print lst, target, avg
+    # print (lst, target, avg)
     while (min < max):
         if (lst[avg] == target):
             return avg
         elif (lst[avg] < target):
+            s = search(lst[avg+1:], target)
+            if s is None:
+                return None
             return avg + 1 + search(lst[avg+1:], target)
         else:
-            return search(lst[:avg], target)
+            s = search(lst[:avg], target)
+            if s is None:
+                return None
+            return s
 
     # avg may be a partial offset so no need to print it here
     # print "The location of the number in the array is", avg
     return avg
 
+names_1.sort()
+s1 = names_1
+# print('Ah search', search(s1, "Ahmad Watts"))
 
 start_time = time.time()
 duplicates = []
-s1 = names_1.sort()
+
 for name_2 in names_2:
     if search(s1, name_2) is not None:
         duplicates.append(name_2)
@@ -60,3 +69,5 @@ for name_2 in names_2:
 end_time = time.time()
 print(f"{len(duplicates)} duplicates list only:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime list only: {end_time - start_time} seconds")
+
+# for list only runtime complexity is O(n log n), space complexity is O(n)
