2f4a17bc9233a5a194ca4aaaa37b6f3fbaf1e98b
willieino <willieino@hotmail.com>
2019-06-15 13:00:15 -0700

    clean code

--- names/names.py
+++ names/names.py
@@ -13,9 +13,6 @@ def namez():
     names_2 = f.read().split("\n")  # List containing 10000 names
     f.close()
       
-    #duplicates = []
-    #names_1.sort()
-    #names_2.sort()
     
     def match(arr, name1, leftIndex, rightIndex):
         # Base case 
@@ -41,35 +38,7 @@ def namez():
     names_2.sort()
     for name_1 in names_1:
         match(names_2, name_1, 0, len(names_2))
-    
-    #while x < names_1.len():
-    #for name_1 in names_1:
-    #    name_1 = names_1[x]
-    #    while names_2 is not None:
-    #        mid = floor(names_2.len()) / 2
-    #        name_2 = names_2[mid]
-    #        if name_2 = name_1:
-    #            duplicates.append(name_1)   
-    #        elif name_1 < name_2:
-    #            mid
-        
-    #    left = names_2.splice(0
-    #    right = names_2[mid+1:]
-    #    name_2 = names_2[mid]
-    #    if name_2 = name_1:
-    #        duplicates.append(name_1)   
-    #    elif name_1 < name_2:
-    #        mid
-            
-        
-    #    for name_2 in names_2:
-    #        mid = floor(names_2.len()) / 2
-    #        left = names_2[:mid]
-    #        right = names_2[mid+1:]
-    #        if        
-    #        if name_1 == name_2:
-    #            duplicates.append(name_1)
-
+   
     end_time = time.time()
     print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
     print (f"runtime: {end_time - start_time} seconds")
