11fe1214b1ac688474caaf4ac8236ce2578f7892
Abigayle Franklin <abifanklin@gmail.com>
2019-06-15 12:10:57 -0500

    Name.py runtime down from 5.5 sec to 0.0065 sec

--- names/names.py
+++ names/names.py
@@ -10,11 +10,7 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+duplicates = set(names_1) & set(names_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
