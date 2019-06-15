950d02a9fb9991f5f61c272e5bb2f2fc6e538f63
Abigayle Franklin <abifanklin@gmail.com>
2019-06-15 11:51:14 -0500

    Ring Buffer completet and passing tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,37 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, length):
+        self.length = length
+        self.count = 0
+        self.storage = [None]*length
 
-  def append(self, item):
-    pass
+    def append(self, value):
+        if self.count < self.length:
+            self.storage[self.count] =  value
+            self.count +=1
+        else:
+            self.count = 0
+            self.storage[self.count] = value
+            self.count += 1
+        
+    def get(self):
 
-  def get(self):
-    pass
+      return [i for i in self.storage if i != None]
+
+
+buffer = RingBuffer(3)
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+buffer.get()   # should return ['a', 'b', 'c']
+
+buffer.append('d')
+
+buffer.get()   # should return ['d', 'b', 'c']
+
+buffer.append('e')
+buffer.append('f')
+
+buffer.get()   # should return ['d', 'e', 'f']
+
