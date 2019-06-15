cf12a873fcbeb3199437646c0b44cb2b209d47f9
willieino <willieino@hotmail.com>
2019-06-15 11:02:04 -0700

    finished q and a

--- names/names.py
+++ names/names.py
@@ -1,22 +1,26 @@
 import time
+from functools import lru_cache
 
-start_time = time.time()
+@lru_cache(maxsize=0)
+def namez():
+    start_time = time.time()
 
-f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
+    f = open('names_1.txt', 'r')
+    names_1 = f.read().split("\n")  # List containing 10000 names
+    f.close()
 
-f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-f.close()
+    f = open('names_2.txt', 'r')
+    names_2 = f.read().split("\n")  # List containing 10000 names
+    f.close()
+      
+    duplicates = []
+    for name_1 in names_1:
+        for name_2 in names_2:
+            if name_1 == name_2:
+                duplicates.append(name_1)
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    end_time = time.time()
+    print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+    print (f"runtime: {end_time - start_time} seconds")
 
-end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+namez()
