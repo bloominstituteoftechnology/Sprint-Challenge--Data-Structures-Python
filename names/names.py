17291463ee0ed7f19cc134977e3a08186b167299
Abigayle Franklin <abifanklin@gmail.com>
2019-06-15 13:11:47 -0500

    Used BST to implement names in stretch - very inefficient. 34 sec

--- names/names.py
+++ names/names.py
@@ -16,3 +16,56 @@ end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+### Stretch ###
+
+start_time2 = time.time()
+
+class BinarySearchTree:
+  def __init__(self, value):
+    self.value = value
+    self.left = None
+    self.right = None
+
+  def insert(self, value):
+    if value < self.value:
+      if not self.left:
+        self.left = BinarySearchTree(value)
+      else:
+        self.left.insert(value)
+    elif value > self.value:
+      if not self.right:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
+
+  def contains(self, target):
+    if target == self.value:
+      return True
+    elif self.right is None and self.left is None:
+      return False
+    else:
+      if self.left is not None:
+        if self.left.contains(target):
+          return True
+      if self.right is not None:
+        if self.right.contains(target):
+          return True
+
+namesOne = BinarySearchTree('Abi')
+
+for i in names_1:
+    namesOne.insert(i)
+
+common = []
+
+for i in names_2:
+    if namesOne.contains(i):
+        common.append(i)
+
+end_time2 = time.time()
+
+###Hahaha - works, but not at all efficient
+print(common)
+print (f"runtime: {end_time2 - start_time2} seconds")
+
+
