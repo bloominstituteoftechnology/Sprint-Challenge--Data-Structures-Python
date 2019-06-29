ea0dfae50f398a67c4816a6c2cf4ecb19ea4616d
Emmanium <pradoe.ep@gmail.com>
2019-06-29 11:32:06 -0600

    finished append method in RingBuffer.py and answered relevant questions in md

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,15 @@ class RingBuffer:
         self.storage = [None]*capacity
 
     def append(self, item):
-        pass
+      # checks to see if the current is less than the ring buffer capacity, if it is then it appends it to the end of the list
+        if self.current < self.capacity:
+            self.storage[self.current] = item
+            self.current += 1
+      # else, it overwrites the oldest value in the list, starting with index 0, then increments it to be ready to overwrite the next value in the list
+        else:
+            self.current = 0
+            self.storage[self.current] = item
+            self.current += 1
 
     def get(self):
         return [i for i in self.storage if i != None]
