883a9358ea20b492181f4b6312c9cf68539672c8
Emmanium <pradoe.ep@gmail.com>
2019-06-29 12:25:45 -0600

    updated the names.py method

--- names/names.py
+++ names/names.py
@@ -50,11 +50,22 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+binarySearchTree = BinarySearchTree(names_2[0])
+for name_1 in names_1[1:]:
+    binarySearchTree.insert(name_1)
+
+# uses the binary seach tree to see if the name is a duplicate and if it is
+# it appends it to the duplicates list
+for name_2 in names_2:
+    if binarySearchTree.contains(name_2):
+        duplicates.append(name_2)
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
