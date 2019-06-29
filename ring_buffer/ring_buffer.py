5a96b246f870b4531c33994b6cc688b1aa8e218b
vy3191 <venkyafhs@gmail.com>
2019-06-29 12:43:38 -0400

    Completed append and get methods in ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,8 +4,22 @@ class RingBuffer:
     self.current = 0
     self.storage = [None]*capacity
   def append(self, item):  
-    self.storage.append(item)  
+    # As long as self.current value is less than self.capacity value
+    # keep adding the item to the storage
+    if self.current < self.capacity:
+      self.storage[self.current] = item
+      # Increase the count value by 1
+      self.current +=1
+      #or else...make the self.current value zero then keep adding the items to the storage  
+    else:
+      self.current = 0
+      self.storage[self.current] = item
+      self.current +=1
 
   def get(self):
-    pass
+    arr = []
+    for item in self.storage:
+      if item != None:
+        arr.append(item)
+    return arr    
     
