b82c43020cc2bfbc992ffab1f51d3a83eea318f9
Jason Hedrick <jasonleehedrick@gmail.com>
2019-06-29 08:41:41 -0700

    Began working on the RingBuffer append method. Initial commit.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,3 +1,10 @@
+# RingBuffer needs to send back any non-None values.
+# Appending to RingBuffer fills up its capacity first
+# and then restarts at the first index overwriting values
+
+# I can use the current variable to maintain what index
+# we can currently assign values to. And if it is greater
+# than the capacity reset it to 0
 class RingBuffer:
   def __init__(self, capacity):
     self.capacity = capacity
@@ -5,6 +12,14 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
+    # Add the value
+    self.storage[current] = item
+    # Increment the value we're currently inserting at
+    self.current += 1
+    # If we're now trying to insert outside of the capacity
+    # of our storage reset it.
+    if (self.current > self.capacity): 
+      self.current = 0
     pass
 
   def get(self):
