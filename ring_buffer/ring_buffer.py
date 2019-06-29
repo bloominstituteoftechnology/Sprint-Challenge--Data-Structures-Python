0ca6dc9c0d170d26e05211f4d0ed47f65f2bce16
Emmanium <pradoe.ep@gmail.com>
2019-06-29 10:34:06 -0600

    initial commit finished get method in ring_buffer.py, answered relevant questions in md

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,11 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        pass
 
-  def get(self):
-    pass
+    def get(self):
+        return [i for i in self.storage if i != None]
