37809e40ac85168723ab96b3fd44977dce1c10f3
vy3191 <venkyafhs@gmail.com>
2019-06-29 12:07:14 -0400

    Initial Commit

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -3,9 +3,9 @@ class RingBuffer:
     self.capacity = capacity
     self.current = 0
     self.storage = [None]*capacity
-
-  def append(self, item):
-    self.storage.append(item)
+  def append(self, item):  
+    self.storage.append(item)  
 
   def get(self):
+    pass
     
