9d9c363fd33a13a45fee330e2d6f370fd5bf12a5
Alex Martin <atonymartin20@yahoo.com>
2019-06-28 20:53:58 -0500

    Completed ring Buffer

--- Data_Structures_Answers.md
+++ Data_Structures_Answers.md
@@ -1,13 +1,16 @@
 Add your answers to the questions below.
 
 1. What is the runtime complexity of your ring buffer's `append` method?
+    A. O(1)
 
 2. What is the space complexity of your ring buffer's `append` function?
+    A. O(1)
 
 3. What is the runtime complexity of your ring buffer's `get` method?
+    A. O(n)
 
 4. What is the space complexity of your ring buffer's `get` method?
-
+    A. O(n)
 
 5. What is the runtime complexity of the provided code in `names.py`?
 
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,18 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+# The `append` method adds elements to the buffer.
+
+    # Change value at self.current to the item's value
+      self.storage[self.current] = item
+    # increment self.current
+      self.current += 1
+
+    # When self.current is the same as self.capacity reset self.current
+      if self.current == self.capacity:
+        self.current = 0
 
   def get(self):
-    pass
+# The `get` method returns all of the elements in the buffer in a list in their given order. 
+# It should not return any `None` values in the list even if they are present in the ring buffer.
+    return [ i for i in self.storage if i != None]
