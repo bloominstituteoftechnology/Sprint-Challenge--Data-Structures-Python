53f175f76b43e2e2c1dd7a978d6f5aa89ada7782
iepoch <steaclem@gmail.com>
2019-06-29 09:29:56 -0700

    - Added some Answers - Tried two versions for names.py I like list better

--- Data_Structures_Answers.md
+++ Data_Structures_Answers.md
@@ -2,12 +2,19 @@ Add your answers to the questions below.
 
 1. What is the runtime complexity of your ring buffer's `append` method?
 
+   > This is O(1). Ring buffers must have a fixed length size at all times.
+
 2. What is the space complexity of your ring buffer's `append` function?
+
+   > This is also true here O(1) it only overwrites the oldest
 
 3. What is the runtime complexity of your ring buffer's `get` method?
 
+   > Again The ring buffers get method still will be an O(n)
+
 4. What is the space complexity of your ring buffer's `get` method?
 
+   > Because of using a loop this complexity will be O(n)
 
 5. What is the runtime complexity of the provided code in `names.py`?
 
--- names/names.py
+++ names/names.py
@@ -10,11 +10,27 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = BinarySearchTree()
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# This will return the duplicates while checking both files
+duplicates = list(set(names_1) & set(names_2))
+
+# This will return if there is existing duplicates in one file
+# bst = BinarySearchTree([names_1[0], 1], [names_2[0], 1])
+# duplicates = []
+# def callback(i):
+#     if i[1] >= 2:
+#         duplicates.append(i[0])
+
+
+# for i in range(1, len(names_1)-1):
+#     bst.insert([names_1[i], 1, 1])
+# for j in names_2:
+#     bst.insert([j, 1, 2])
+
+# bst.for_each(callback)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
