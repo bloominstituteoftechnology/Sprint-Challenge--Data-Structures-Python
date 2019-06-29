1e3791e0f354b83298ce63ac12bdeb45b7c2f71b
iepoch <steaclem@gmail.com>
2019-06-29 08:12:53 -0700

    Initial Commit - Focused on Ring Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,15 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        if len(self.storage) == self.capacity:
+            self.storage[self.current] = item
+        else:
+            self.storage.append(item)
+            self.current = (self.current + 1) % self.capacity
 
-  def get(self):
-    pass
+    def get(self):
+        return self.storage
