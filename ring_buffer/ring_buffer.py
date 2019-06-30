4d44124b8ead697de3ad2f1a5e86cd5c65849e87
Denise Escobar <descobar1490@gmail.com>
2019-06-30 12:17:58 -0500

    Ringbuffer is passing all tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,28 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
+    if None in self.storage:
+      x = self.storage.index(None) 
+      self.storage[x] = item
+      self.current = x
+    else :
+      if self.current == self.capacity-1:
+        x = abs(self.current - (self.capacity-1))
+        self.storage[x] = item
+        self.current = x
+      else: 
+        x = self.current+1
+        self.storage[x] = item
+        self.current = x 
     pass
 
   def get(self):
+    tempList = []
+    if None in self.storage:
+      for x in self.storage:
+        if x != None:
+          tempList.append(x)
+      return tempList
+    else: 
+      return self.storage
     pass
