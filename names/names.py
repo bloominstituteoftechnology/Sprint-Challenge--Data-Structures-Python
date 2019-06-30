9a5a073d786d939d7aca97c94b32740a12dce103
Jerrick Warren <Jerrick.Warren@gmail.com>
2019-06-30 17:57:14 -0400

    used set for names runtime

--- names/names.py
+++ names/names.py
@@ -10,11 +10,11 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+#  use of set 
+#  makes is really fast
+#  https://www.programiz.com/python-programming/set 
+
+duplicates = set(names_1) & set(names_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
