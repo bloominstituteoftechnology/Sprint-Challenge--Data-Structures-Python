9105b4d4987da111562e154c337ba9925ba8046d
Michael Trew <mtrew2015@gmail.com>
2019-06-30 01:14:46 -0400

    working in finding a better solution for anames

--- names/names.py
+++ names/names.py
@@ -10,11 +10,13 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+name_set = set(names_1).intersection(set(names_2))
+duplicates=[]
+for i in name_set:
+    num = (min(names_1.count(i), names_2.count(i)))
+    for j in range(num):
+        duplicates.append(i)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
