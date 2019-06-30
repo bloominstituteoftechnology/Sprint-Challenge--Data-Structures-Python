03c3b6ab5224bccc9666f0ac95bc25b5f0c211b0
Salzberg <Robert.Salzberg@utsad.com>
2019-06-30 15:10:42 -0400

    Added append method

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,23 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        # Keep adding the item to the storage
+        # if current value is less than capacity value
+        if self.current < self.capacity:
+            self.storage[self.current] = item
+        # Increase the count value by 1
+            self.current += 1
+        # If current value is greater than capacity
+        # reset current value zero and 
+        # then keep adding the items to the storage
+        else:
+            self.current = 0
+            self.storage[self.current] = item
+            self.current += 1
 
-  def get(self):
-    pass
+    def get(self):
+      pass
