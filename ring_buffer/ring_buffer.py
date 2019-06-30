1583c03dbe3e70179d326e759609e9b9d2de08bd
Kendra Williams <kmonique.williams@gmail.com>
2019-06-30 11:23:49 -0400

    general functionality of ring buffer working

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,24 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    #check to see if current is too large
+    #pop old item off end
+    #insert new item at currents position
+    if self.current >= self.capacity:
+      self.current = 0
+    self.storage.pop()
+    self.storage.insert(self.current, item)
+    self.current +=1
 
   def get(self):
-    pass
+    #only return if not none
+    return [item for item in self.storage if item != None]
+
+buffer = RingBuffer(3)
+print(buffer.get())
+
+buffer.append("a")
+buffer.append("b")
+buffer.append("c")
+
+print(buffer.get())
