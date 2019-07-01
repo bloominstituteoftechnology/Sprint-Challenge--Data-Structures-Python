165c9225ae08d078c19492dcfd23c0234fe22366
Barbara Hunter <commandandcode@gmail.com>
2019-06-30 22:32:05 -0400

    fixed names and finished questions

--- names/names.py
+++ names/names.py
@@ -11,17 +11,13 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+names_1 = set(names_1) # runtime of 0.0072 seconds
+
 duplicates = []
-bst = BinarySearchTree(names_2[0])
-
-for name_2 in names_2[1:]:
-    bst.insert(name_2)
-
-for name_1 in names_1:
-    if bst.contains(name_1):
-        duplicates.append(name_1)
+for name in names_2:
+    if name in names_1:
+        duplicates.append(name) # runtime 0.006
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-
