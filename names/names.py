74c8057cdbb2f7128ee44e743da4cda22b9455a7
Jennifer King <jenbellpgh@gmail.com>
2019-07-01 17:42:21 -0400

    Tried stretch, worse result

--- names/names.py
+++ names/names.py
@@ -11,11 +11,14 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# Original code -- Runtime: 12.05 seconds:
 # duplicates = []
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
+
+# Optimized code using a BST -- Runtime: 0.19s
 bst = None
 
 for name1 in names_1:
@@ -29,6 +32,15 @@ duplicates = []
 for name2 in names_2:
     if bst.contains(name2):
         duplicates.append(name2)
+
+# Code for stretch -- Runtime: 2.43s:
+# names = [name1 for name1 in names_1]
+
+# duplicates = []
+
+# for name2 in names_2:
+#     if name2 in names:
+#         duplicates.append(name2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
