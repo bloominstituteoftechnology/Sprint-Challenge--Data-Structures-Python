03ee110ff831cb9c8f8a930fda4b5eb8d97151d0
Robert Billington <chillingtonbillington@gmail.com>
2019-07-03 10:41:37 -0500

    First pass ring buffer, append works, get works, append failing with final index case

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,35 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    #if we are past last index, reset to beginning
+    if self.current > self.capacity:
+      self.current = 0
+    #sets storage to new incoming item  
+    self.storage[self.current] = item
+    #increases index
+    self.current += 1
+    print(self.storage)  
 
   def get(self):
-    pass
+    print([i for i in self.storage if i != None])
+    return [i for i in self.storage if i != None]
+
+buffer = RingBuffer(3)
+
+buffer.get()   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+
+buffer.get()   # should return ['a', 'b', 'c']
+
+# 'd' overwrites the oldest value in the ring buffer, which is 'a'
+buffer.append('d')
+
+buffer.get()   # should return ['d', 'b', 'c']
+
+buffer.append('e')
+buffer.append('f')
+
+buffer.get()   # should return ['d', 'e', 'f']    
