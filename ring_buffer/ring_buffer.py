320e3b3b8363c69e9cc1e90a4fd8979a9e02319b
hraya <hraya@lssu.edu>
2019-07-03 23:34:50 -0500

    Initial commit
    Completed ring_buffer, test pass

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,12 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    self.storage[self.current] = item
+    if self.current == len(self.storage)-1:
+      self.current = 0
+    else:
+      self.current += 1
 
   def get(self):
-    pass
+    value = [val for val in self.storage if val != None]
+    return value
