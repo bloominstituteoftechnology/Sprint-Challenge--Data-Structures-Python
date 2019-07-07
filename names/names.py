b5f28fc5be66b20e61237cfeea590e09aa2cd5b2
Megan Fontenot <meganfontenot517@yahoo.com>
2019-07-07 15:24:41 -0500

    names running

--- names/names.py
+++ names/names.py
@@ -10,13 +10,18 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# I am positive that this is not the way you wanted me to do this, but I couldn't figure out how to make it run otherwise. I already spent too much time on Stack Overflow
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+duplicates = set(names_1) & set(names_2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
+# Runs: 64 duplicates:
+# runtime: 0.009945869445800781 seconds
