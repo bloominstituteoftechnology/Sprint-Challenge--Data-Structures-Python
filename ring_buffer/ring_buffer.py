d3f5221e664b642416482280424b3e6954462042
Megan Fontenot <meganfontenot517@yahoo.com>
2019-07-07 14:29:19 -0500

    ring-buffer passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,20 @@
+# RingBuffer has two methods, append and get
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+# The append method adds elements to the buffer
+    def append(self, item):
+        self.storage[self.current] = item
+        if self.current >= len(self.storage)-1:
+            self.current = 0
+        else:
+            self.current += 1
 
-  def get(self):
-    pass
+# The get method returns all of the elements in the buffer in a list in their given order.
+    def get(self):
+        return [v for v in self.storage if v is not None]
+
+# Test: Ran 1 test in 0.000s
