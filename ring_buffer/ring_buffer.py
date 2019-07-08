6b5fe62b0db91c50084807ac440fd0a45242b27a
Pascale Pierre <pascaleee@gmail.com>
2019-07-07 22:39:20 -0500

    completed sprint challenge

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,12 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
+    self.storage[self.current] = item
+    self.current +=1
+    if self.current >= self.capacity:
+        self.current = 0
     pass
 
   def get(self):
-    pass
+    return [x for x in self.storage if x != None]
+   
