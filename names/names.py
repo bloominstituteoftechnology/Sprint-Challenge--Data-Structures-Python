3f49a65c9d9e9c646f1833711df56e33a40681d0
Jorlost19 <jorlost19@gmail.com>
2019-07-08 21:47:07 -0500

    finished stretch goal or I think so.

--- names/names.py
+++ names/names.py
@@ -16,3 +16,9 @@ end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+# This solution is better than the solution above because 
+# its not using any extra space, it's returning the duplicates names
+# straight from the 2 original lists, in the above solution I created a third list
+# to get the job done, in this one my space complexity is O(n).
+
+print (f"stretch goal: {', '.join(set(names_1).intersection(names_2))}")
