937182a6e3c5f4e6f732fda45ff1e46078d39787
unknown <jerrardsmith215@gmail.com>
2019-07-08 23:06:23 -0400

    ringbuffer append logic complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,6 +5,12 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
+    # check if the buffer has been filled
+    if len(self.storage) >= self.capacity:
+      self.current = 0 # if so, set current index to 0
+    else: 
+      self.storage[self.current] = item # otherwise, set current index item equal to passed-in item
+      self.current += 1 # then increment to next index
     pass
 
   def get(self):
