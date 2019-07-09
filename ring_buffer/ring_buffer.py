e535693521e6b34b36395b86e9bd632034176b9c
unknown <jerrardsmith215@gmail.com>
2019-07-09 00:03:50 -0400

    ring_buffer completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,13 +5,25 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
+    print('passed in item: ' + item)
+    print('current index: ')
+    print(self.current)
     # check if the buffer has been filled
-    if len(self.storage) >= self.capacity:
+    if self.current > self.capacity-1:
       self.current = 0 # if so, set current index to 0
-    else: 
+      self.storage[self.current] = item # overwrite existing item
+      self.current +=1 # increment index by 1
+    else:
       self.storage[self.current] = item # otherwise, set current index item equal to passed-in item
       self.current += 1 # then increment to next index
-    pass
+    print('current storage: ')
+    print(self.storage)
 
   def get(self):
-    pass
+    # create a placeholder variable as an empty list
+    newList = []
+    # iterate current storage
+    for i in range(self.capacity): # append item to the placeholder if != None
+      if self.storage[i] != None:
+        newList.append(self.storage[i])
+    return newList
