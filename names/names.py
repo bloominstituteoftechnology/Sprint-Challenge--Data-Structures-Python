c9462d84e804302459f0be709ab8ed5a2a6b92cf
Jorlost19 <jorlost19@gmail.com>
2019-07-08 21:06:01 -0500

    refactored names.py use .intersection instead of &

--- names/names.py
+++ names/names.py
@@ -10,7 +10,7 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")
 f.close()
 
-duplicates = set(names_1) & set(names_2)
+duplicates = set(names_1).intersection(names_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
