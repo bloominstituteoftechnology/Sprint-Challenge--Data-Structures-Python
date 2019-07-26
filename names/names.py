5f716029e1bf128df8f94ff6c10c71226206ecc1
Cole Hudson <cole@colejhudson.com>
2019-07-26 10:02:06 -0700

    added names exercise

--- names/names.py
+++ names/names.py
@@ -2,21 +2,55 @@ import time
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
+with open('names_1.txt', 'r') as f:
+    names_1 = f.read().split("\n")  # List containing 10000 names
 
-f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-f.close()
+with open('names_2.txt', 'r') as f:
+    names_2 = f.read().split("\n")  # List containing 10000 names
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.depth = 0
+
+    def search(self, string):
+        c = string[0]
+
+        if c in self.children:
+            if len(string) == 1:
+                return self.children[c]
+            else:
+                return self.children[c].search(string[1:])
+        else:
+            return self
+
+    def insert(self, string):
+        if len(string) == 0: return
+
+        c = string[0]
+
+        if c not in self.children:
+            self.children[c] = Trie()
+
+        self.children[c].insert(string[1:])
+
+    def insert_many(self, xs):
+        for x in xs: self.insert(x)
+
+    def contains_prefix(self, string):
+        return self.search(string) is not None
+
+    def contains_exactly(self, string):
+        return {} == self.search(string).children 
+
+    def contains_many(self, strings):
+        return [self.contains_exactly(string) for string in strings]
+
+trie = Trie()
+trie.insert_many(names_1)
+
+duplicates = [name for name in names_2 if trie.contains_exactly(name)]
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-
