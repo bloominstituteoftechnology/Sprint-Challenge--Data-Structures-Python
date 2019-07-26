85ad07f1256e71954fe6c4792e0486bc9689e8ce
Dmitriy <dkav@live.com>
2019-07-26 13:30:19 -0400

    names finished

--- names/names.py
+++ names/names.py
@@ -10,13 +10,21 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+names_all = names_1 + names_2
+
+# Get counts
+counts = {}
+
+for i in names_all:
+  try:
+    counts[i] += 1
+  except:
+    counts[i] = 1
+
+# Duplicates have counts > 1
+duplicates = [k for k,v in counts.items() if v > 1]
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-
