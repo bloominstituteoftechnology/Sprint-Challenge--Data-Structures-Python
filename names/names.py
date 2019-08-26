7024765561b5d2938493ed65e78d789c3c35b91c
jkaddu <jkaddu@yahoo.com>
2019-08-25 18:13:47 -0700

    BST implemented

--- names/names.py
+++ names/names.py
@@ -1,5 +1,54 @@
 import time
 
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else: 
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+
+    def get_max(self):
+        if not self: 
+            return False
+        else:
+            if not self.right:
+                return self.value
+            else:
+                return self.right.get_max()
+
+    def for_each(self, cb):
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -11,20 +60,14 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-# for name_1 in names_1:
-#    for name_2 in names_2:
-#        if name_1 == name_2:
-#            duplicates.append(name_1)
-
-# for name_1 in names_1:
-#    if name_1 in names_2:
-#        duplicates.append(name_1)
-
-# duplicates = set(names_1) & set(names_2)
-
-duplicates = set(names_1) & set(names_2)
+binarySearchTree = BinarySearchTree(names_2[0])
+for name_1 in names_1[1:]:
+    binarySearchTree.insert(name_1)
+for name_2 in names_2:
+    if binarySearchTree.contains(name_2):
+        duplicates.append(name_2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\{', '.join(duplicates)}\n\n")
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
