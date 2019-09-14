033e396cb14a587ef38d351b8770a64b24b23b28
Alando Appling <alandoappling@gmail.com>
2019-09-13 20:57:59 -0700

    Completed Sprint Challenge

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -24,7 +24,7 @@ class LinkedList:
     node = Node(value)
     if self.head is not None:
       node.set_next(self.head)
-    
+
     self.head = node
 
   def contains(self, value):
@@ -32,7 +32,7 @@ class LinkedList:
       return False
     # get a reference to the node we're currently at; update this as we traverse the list
     current = self.head
-    # check to see if we're at a valid node 
+    # check to see if we're at a valid node
     while current:
       # return True if the current value we're looking at matches our target value
       if current.get_value() == value:
@@ -43,5 +43,11 @@ class LinkedList:
     return False
 
   def reverse_list(self):
-    # TO BE COMPLETED
-    pass
+    current = self.head
+    prev = None
+    while current != None:
+      next = current.next_node
+      current.next_node = prev
+      prev = current
+      current = next
+    self.head = prev
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,17 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    self.storage = [i for i in self.storage if i is not None]
+    if len(self.storage) == self.capacity:
+      self.storage[self.current] = item
+      self.current = (self.current + 1) % self.capacity
+
+    if len(self.storage) != self.capacity:
+      self.storage.append(item)
+
+      while len(self.storage) < self.capacity:
+        self.storage.insert(len(self.storage), None)
 
   def get(self):
-    pass
+    self.storage = [i for i in self.storage if i is not None]
+    return self.storage
