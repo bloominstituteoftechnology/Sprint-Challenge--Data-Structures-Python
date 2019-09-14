7f77c61d01ed965810d91a6fdc5372c5bb2d9f2b
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2019-09-14 14:57:37 -0400

    names.py: runs in ~0.17 seconds

--- names/names.py
+++ names/names.py
@@ -1,5 +1,45 @@
 import time
 
+
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left == None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right == None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        elif target < self.value:
+            if self.left is not None:
+                return self.left.contains(target)
+            else:
+                return False
+        elif target > self.value:
+            if self.right is not None:
+                return self.right.contains(target)
+            else:
+                return False
+
+    def get_max(self):
+        if self.right is not None:
+            return self.right.get_max()
+        else:
+            return self.value
+
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -11,12 +51,16 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# create a binary search tree with names from names_1
+BST = BinarySearchTree(names_2[0])
+for name_1 in names_1[1:]:
+    BST.insert(name_1)
+
+# find duplicates of names from names_1 in names_2, append to the duplicates list
+for name_2 in names_2:
+    if BST.contains(name_2):
+        duplicates.append(name_2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
