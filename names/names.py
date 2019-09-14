e367d213cfe496e49a0fbca388b0f35a392f12b8
Bao Pham <ryanvuepham@gmail.com>
2019-09-14 13:13:02 -0500

    names.py

--- names/names.py
+++ names/names.py
@@ -10,13 +10,17 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# run time average around 13 seconds after about 5 runs.
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# runt time is now 0.012 secs
+names_1 = set(names_1)
+duplicates = [x for x in names_2 if x in names_1]
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
-
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
