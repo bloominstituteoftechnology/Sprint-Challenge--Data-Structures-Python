deabf74f43d58ac0278fa8ad67d412f1b1987f33
droneal11015 <droneal11015@gmail.com>
2019-09-20 12:21:03 -0400

    completed ring buffer, working on passing test

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,10 +7,13 @@ class RingBuffer:
   def append(self, item):
     self.storage[self.current] = item
     #if ring buffer is full (available capacity is 0), this adds an element to the array and sets current to the first index
-    if self.current == self.capacity -1:
+    if self.current == self.capacity - 1:
       self.current = 0
-    else:
+    #if ring buffer is not full, adds element to this array
+    elif self.current < self.capacity - 1:
       self.current += 1
 
   def get(self):
+    print([item for item in self.storage if item is not None])
+    
     
