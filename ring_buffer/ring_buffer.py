b56790e6e4124eb3f7211a102266cb08740afc6f
victorN <victornguyen082@gmail.com>
2019-09-20 12:57:16 -0400

    ring buffer test pass

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,22 @@
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-  def append(self, item):
-    pass
+    def append(self, item):  # Adds the element to the buffer
+      # current index is item
+        self.storage[self.current] = item
+        self.current += 1
+        if self.current == self.capacity:
+            self.current = 0  # makes current to 0 and restarts
 
-  def get(self):
-    pass
+    # Returns all the elements in the buffer in a list in their given order. No none values.
+
+    def get(self):
+        buffer = []
+        for i in self.storage:
+            if i != None:
+                buffer.append(i)
+
+        return buffer
