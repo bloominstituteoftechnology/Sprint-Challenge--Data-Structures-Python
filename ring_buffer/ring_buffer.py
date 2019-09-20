b9d0af4f06dac85b732f28b11d4848a9b99f1a80
droneal11015 <droneal11015@gmail.com>
2019-09-20 12:26:05 -0400

    switched statments around, still cannot pass

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,15 +5,20 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    self.storage[self.current] = item
-    #if ring buffer is full (available capacity is 0), this adds an element to the array and sets current to the first index
-    if self.current == self.capacity - 1:
-      self.current = 0
+    self.storage[self.current] = item       
+    
     #if ring buffer is not full, adds element to this array
-    elif self.current < self.capacity - 1:
+    if self.current < self.capacity:
       self.current += 1
+      
+    #if ring buffer is full (available capacity is 0), this adds an element to the array and sets current to the first index
+    elif self.current == self.capacity - 1:
+      self.current = 0
+
+
 
   def get(self):
+    #returns item if index is not None
     print([item for item in self.storage if item is not None])
     
     
