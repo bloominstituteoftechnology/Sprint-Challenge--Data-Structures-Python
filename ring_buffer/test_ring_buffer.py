ebc6a51d2856e857ef11e31b4d22d37548fb19ee
Brian Doyle <briandoyle81@gmail.com>
2019-10-18 16:04:11 -0600

    Update test_ring_buffer.py

--- ring_buffer/test_ring_buffer.py
+++ ring_buffer/test_ring_buffer.py
@@ -4,6 +4,7 @@ from ring_buffer import RingBuffer
 class RingBufferTests(unittest.TestCase):
     def setUp(self):
         self.buffer = RingBuffer(5)
+        self.buffer_2 = RingBuffer(5)
 
     def test_ring_buffer(self):
         self.assertEqual(len(self.buffer.storage), 5)
@@ -32,6 +33,10 @@ class RingBufferTests(unittest.TestCase):
         self.buffer.append('j')
         self.buffer.append('k')
         self.assertEqual(self.buffer.get(), ['k', 'g', 'h', 'i', 'j'])
+        
+        for i in range(50):
+            self.buffer_2.append(i)
+                self.assertEqual(self.buffer2.get(), [45, 46, 47, 48, 49])
 
 
 if __name__ == '__main__':
