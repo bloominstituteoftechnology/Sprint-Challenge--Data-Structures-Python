970f0c268dfa5fd4f139b465cacd4d7b324376e7
Mitchell Robles <mitchellr92@live.com>
2019-10-18 08:21:23 -0700

    Append in ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,16 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    for i in range(len(self.storage)):
+      if self.storage[i] == None:
+        self.storage[i] = item
+
+    self.storage[self.current] = item
+
+    if self.current == self.capacity - 1:
+      self.current = 0
+    else:
+      self.current += 1
 
   def get(self):
     pass
