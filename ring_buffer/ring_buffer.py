bc28f9e79a176f907d9700d5d45d82788706d30c
veritaem <dcharris776@gmail.com>
2019-10-18 12:29:28 -0400

    first problem

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,22 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    self.storage[self.current] = item
+    self.current += 1
+    if self.current == self.capacity:
+      self.current = 0
 
+  # return all elements of cache
   def get(self):
-    pass
+    print(self.storage)
+    return [i for i in self.storage if i]
+
+
+r = RingBuffer(3)
+r.append('a')
+r.append('b')
+r.append('c')
+r.append('d')
+r.append('e')
+r.append('f')
+r.append
