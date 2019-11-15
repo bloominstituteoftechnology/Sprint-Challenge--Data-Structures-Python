7ae65facfd8030d1d8c615c4894f45e8d48b2b94
danpatrickoneill <dan47573@gmail.com>
2019-11-15 12:05:25 -0500

    Fixed indentation error

--- ring_buffer/test_ring_buffer.py
+++ ring_buffer/test_ring_buffer.py
@@ -1,6 +1,7 @@
 import unittest
 from ring_buffer import RingBuffer
 
+
 class RingBufferTests(unittest.TestCase):
     def setUp(self):
         self.buffer = RingBuffer(5)
@@ -29,14 +30,14 @@ class RingBufferTests(unittest.TestCase):
         self.buffer.append('i')
         self.assertEqual(len(self.buffer.storage), 5)
         self.assertEqual(self.buffer.get(), ['f', 'g', 'h', 'i', 'e'])
-        
+
         self.buffer.append('j')
         self.buffer.append('k')
         self.assertEqual(self.buffer.get(), ['k', 'g', 'h', 'i', 'j'])
-        
+
         for i in range(50):
             self.buffer_2.append(i)
-            self.assertEqual(self.buffer_2.get(), [45, 46, 47, 48, 49])
+        self.assertEqual(self.buffer_2.get(), [45, 46, 47, 48, 49])
 
 
 if __name__ == '__main__':
