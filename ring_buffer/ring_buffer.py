38a28874069fd1c4f62ff2f0686cffa438053f77
briandoyle81 <briandoyle81@gmail.com>
2019-11-18 10:54:25 -0600

    update starter for LL and 4 spaces

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,11 +1,32 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class RingBuffer:
-  def __init__(self, capacity):
-    self.capacity = capacity
-    self.current = 0
-    self.storage = [None]*capacity
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.current = None
+        self.storage = DoublyLinkedList()
 
-  def append(self, item):
-    pass
+    def append(self, item):
+        pass
 
-  def get(self):
-    pass
+    def get(self):
+        # Note:  This is the only [] allowed
+        list_buffer_contents = []
+
+        # TODO: Your code here
+
+        return list_buffer_contents
+
+# ----------------Stretch Goal-------------------
+
+
+class ArrayRingBuffer:
+    def __init__(self, capacity):
+        pass
+
+    def append(self, item):
+        pass
+
+    def get(self):
+        pass
