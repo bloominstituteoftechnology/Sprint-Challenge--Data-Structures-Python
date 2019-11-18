b6f92d6d76e7b05efd9a3a1fceeb29c661aae2d4
briandoyle81 <briandoyle81@gmail.com>
2019-11-18 11:25:49 -0600

    update tests

--- ring_buffer/test_ring_buffer.py
+++ ring_buffer/test_ring_buffer.py
@@ -1,5 +1,6 @@
 import unittest
-from ring_buffer import RingBuffer
+from ring_buffer import RingBuffer, ArrayRingBuffer
+
 
 class RingBufferTests(unittest.TestCase):
     def setUp(self):
@@ -7,6 +8,44 @@ class RingBufferTests(unittest.TestCase):
         self.buffer_2 = RingBuffer(5)
 
     def test_ring_buffer(self):
+        self.assertEqual(self.buffer.storage.length, 0)
+
+        self.buffer.append('a')
+        self.buffer.append('b')
+        self.buffer.append('c')
+        self.buffer.append('d')
+        self.assertEqual(self.buffer.storage.length, 4)
+        self.assertEqual(self.buffer.get(), ['a', 'b', 'c', 'd'])
+
+        self.buffer.append('e')
+        self.assertEqual(self.buffer.storage.length, 5)
+        self.assertEqual(self.buffer.get(), ['a', 'b', 'c', 'd', 'e'])
+
+        self.buffer.append('f')
+        self.assertEqual(self.buffer.storage.length, 5)
+        self.assertEqual(self.buffer.get(), ['f', 'b', 'c', 'd', 'e'])
+
+        self.buffer.append('g')
+        self.buffer.append('h')
+        self.buffer.append('i')
+        self.assertEqual(self.buffer.storage.length, 5)
+        self.assertEqual(self.buffer.get(), ['f', 'g', 'h', 'i', 'e'])
+
+        self.buffer.append('j')
+        self.buffer.append('k')
+        self.assertEqual(self.buffer.get(), ['k', 'g', 'h', 'i', 'j'])
+
+        for i in range(50):
+            self.buffer_2.append(i)
+        self.assertEqual(self.buffer_2.get(), [45, 46, 47, 48, 49])
+
+
+class ArrayRingBufferTests(unittest.TestCase):
+    def setUp(self):
+        self.buffer = ArrayRingBuffer(5)
+        self.buffer_2 = ArrayRingBuffer(5)
+
+    def test__array_ring_buffer(self):
         self.assertEqual(len(self.buffer.storage), 5)
 
         self.buffer.append('a')
@@ -29,11 +68,7 @@ class RingBufferTests(unittest.TestCase):
         self.buffer.append('i')
         self.assertEqual(len(self.buffer.storage), 5)
         self.assertEqual(self.buffer.get(), ['f', 'g', 'h', 'i', 'e'])
-        
-        self.buffer.append('j')
-        self.buffer.append('k')
-        self.assertEqual(self.buffer.get(), ['k', 'g', 'h', 'i', 'j'])
-        
+
         for i in range(50):
             self.buffer_2.append(i)
         self.assertEqual(self.buffer_2.get(), [45, 46, 47, 48, 49])
