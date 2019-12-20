4fa5aee21b0a866fa94e40cb678801101c6cd0b4
Nathan Van Wyck <nathanvanwyck@Macintosh-00254bbd8f8a-2.local>
2019-12-20 14:00:40 -0500

    Completed Sprint Challenge

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,34 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
+        elif self.storage.length == self.capacity:
+            to_delete = self.storage.head
+            self.storage.remove_from_head()
+            self.storage.add_to_tail(item)
+            if to_delete == self.current:
+                self.current = self.storage.tail
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        initial_node = self.current
+        list_buffer_contents.append(initial_node.value)
+
+        if initial_node.next is not None:
+            next_node = initial_node.next
+        else:
+            next_node = self.storage.head
+
+        while next_node != initial_node:
+            list_buffer_contents.append(next_node.value)
+            if next_node.next is not None:
+                next_node = next_node.next
+            else:
+                next_node = self.storage.head
 
         return list_buffer_contents
 
