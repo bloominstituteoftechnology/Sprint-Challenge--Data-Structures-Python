4da766b017369e5118ca79c828cd391590eed6f7
David <digital0david@gmail.com>
2020-01-11 16:22:26 -0500

    names

--- names/names.py
+++ names/names.py
@@ -10,15 +10,44 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+x = sorted(names_1)
+y = sorted(names_2)
+
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+def binarySearch(arr, target):
+    start = 0 
+    end = len(arr) - 1
+
+    while start <= end:
+        middle = (start + end) // 2
+        midpoint = arr[middle]
+        if midpoint > target:
+            end = middle - 1
+        elif midpoint < target:
+            start = middle + 1
+        else: 
+            return midpoint
+
+for name_1 in x:
+   names = binarySearch(y, name_1)
+   if name_1 == names:
+       duplicates.append(name_1)
+
+
+# duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# Runtime is O(n^2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
