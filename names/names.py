8311e37601e6e53d49fdef532f8c2f25c392978c
Brian Doyle <briandoyle81@gmail.com>
2020-01-31 11:41:52 -0600

    Update names.py

--- names/names.py
+++ names/names.py
@@ -10,7 +10,9 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []
+duplicates = []  # Return the list of duplicates in this data structure
+
+# Replace the nested for loops below with your improvements
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
