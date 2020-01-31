14e0f2fde5f9cff8cd1fd0ac86a229717e6958e3
Michael Redig <mredig@gmail.com>
2020-01-31 12:09:13 -0600

    got ring buffer working buffering entire array at init

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -46,19 +46,14 @@ class RingBuffer:
 
 class ArrayRingBuffer:
     def __init__(self, capacity):
-        self.capacity = capacity
         self.current = -1
-        self.storage = []
+        self.storage = [None] * capacity
 
     def append(self, item):
-        if len(self.storage) < self.capacity:
-            self.storage.append(item)
-            self.current += 1
-        else:
-            self.current += 1
-            if self.current == self.capacity:
-                self.current = 0
-            self.storage[self.current] = item
+        self.current += 1
+        if self.current == len(self.storage):
+            self.current = 0
+        self.storage[self.current] = item
 
     def get(self):
-        return self.storage
+        return [x for x in self.storage if x is not None]
