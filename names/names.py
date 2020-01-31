33ecaaa7cea7e5c89cc4d73a0e8710770c78182c
Brian Doyle <briandoyle81@gmail.com>
2020-01-31 12:35:26 -0600

    Update names.py

--- names/names.py
+++ names/names.py
@@ -24,5 +24,5 @@ print (f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish with no restrictions on techniques or data
-# structures?
+# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# structures, but you may not import any additional libraries that you did not write yourself.
