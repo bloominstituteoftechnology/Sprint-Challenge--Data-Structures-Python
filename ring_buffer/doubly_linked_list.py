165e35d654b254f760eba91c09d094a5198fac23
Michael Redig <mredig@gmail.com>
2020-01-31 11:35:47 -0600

    ring buffer working

--- ring_buffer/doubly_linked_list.py
+++ ring_buffer/doubly_linked_list.py
@@ -92,6 +92,12 @@ class DoublyLinkedList:
         self.delete(self.tail)
         return value
 
+    def insert_after(self, existingNode, newValue):
+        existingNode.insert_after(newValue)
+        self.length += 1
+        if existingNode == self.tail:
+            self.tail = self.tail.next
+
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
