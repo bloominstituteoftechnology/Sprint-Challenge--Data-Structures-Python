c46bbfc4e0ee56551cce24f7352ea22b95d17a24
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2020-02-22 10:38:12 -0800

    binary seach tree

--- names/names.py
+++ names/names.py
@@ -1,5 +1,50 @@
 import time
 
+# copy pasta BST (binary search tree) we did in the lesson
+
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None 
+
+    def insert(self, value):
+        if self.value < value:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
+        elif self.value >= value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif self.right and self.value < target:
+            return self.right.contains(target)
+        elif self.left and self.value > target:
+            return self.left.contains(target)
+        else: 
+            return False
+
+    def get_max(self):
+        if self.right is not None:
+            return self.right.get_max()
+        else: 
+            return self.value
+
+    def for_each(self, cb):
+        cb(self.value)
+
+        if self.left is not None:
+            self.left.for_each(cb)
+        if self.right is not None:
+            self.right.for_each(cb)
+
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
