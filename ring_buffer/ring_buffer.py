31b45baeb9e6efa7daecb9cef3117d39d439c1ee
Coop Williams <ken.c.williams.iii@gmail.com>
2020-02-28 12:30:21 -0500

    append items to RingBuffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,11 +4,25 @@ from doubly_linked_list import DoublyLinkedList
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = None
+        # total items ever appended
+        self.current = 0
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+        else:
+            replacing = self.storage.head
+            for i in range(self.current % self.capacity):
+                replacing = replacing.next
+            replacing.insert_before(item)
+            temp = replacing
+            if replacing == self.storage.head:
+                self.storage.head = replacing.prev
+            if replacing == self.storage.tail:
+                self.storage.tail = replacing.prev
+            temp.delete()
+        self.current += 1
 
     def get(self):
         # Note:  This is the only [] allowed
