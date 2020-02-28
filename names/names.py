be151011c386cbe92b3a0c30863d720c1339c1ea
scottwmwork <scottwmwork@users.noreply.github.com>
2020-02-28 09:39:41 -0700

    added BinarySearchTree.py

--- names/names.py
+++ names/names.py
@@ -13,11 +13,17 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+
 # NOTE below is O(n^2)
+
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
+
+for name in names_1:
+    if names_2.count(name) > 0:
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -27,3 +33,6 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+
+
