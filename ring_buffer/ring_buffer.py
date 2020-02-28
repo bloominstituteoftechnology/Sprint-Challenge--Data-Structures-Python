0273be3ffa08cd19cb353e88de186ae28a0060ab
scottwmwork <scottwmwork@users.noreply.github.com>
2020-02-28 09:04:12 -0700

    finished ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,34 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+
+        # If capacity is full
+        if self.storage.length == self.capacity:
+            # Replace oldest
+            self.current.value = item
+            # Reset oldest
+            self.current = self.current.next
+        else:
+            # Add item to tail (most recent)
+            self.storage.add_to_tail(item)
+            
+            if self.storage.length == self.capacity:
+                self.storage.tail.next = self.storage.head
+            else:
+                # Reset oldeset
+                self.current = self.storage.head
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        count = 0
+        node = self.storage.head
+        while count != self.storage.length:
+            count += 1
+            list_buffer_contents.append(node.value)
+            node = node.next
 
         return list_buffer_contents
 
@@ -30,3 +51,17 @@ class ArrayRingBuffer:
 
     def get(self):
         pass
+
+
+buffer = RingBuffer(3)
+
+buffer.get()   # should return []
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+buffer.append('d')
+buffer.append('e')
+buffer.append('f')
+buffer.append('s')
+print(buffer.get())  # should return ['a', 'b', 'c']
