44b9f50fb916c36ab0015b1911b17cf07e237e86
scottwmwork <scottwmwork@users.noreply.github.com>
2020-02-28 11:03:29 -0700

    initial

--- names/names.py
+++ names/names.py
@@ -15,7 +15,6 @@ duplicates = []  # Return the list of duplicates in this data structure
 # Replace the nested for loops below with your improvements
 
 # NOTE below is O(n^2)
-
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
