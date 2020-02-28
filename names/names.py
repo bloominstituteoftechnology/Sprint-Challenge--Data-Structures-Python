a288e9831895aaf66808f41757977f2c813c3336
Coop Williams <ken.c.williams.iii@gmail.com>
2020-02-28 13:04:35 -0500

    find duplicates in 0.0029 secs

--- names/names.py
+++ names/names.py
@@ -11,6 +11,21 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# # Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+'''The above code runs in O(n^2) time.
+For any inputs, the code must calculate
+the cartesian product of the two lists.
+In the worst-case scenario, the lists
+are of equal length.
+
+My answer (below) runs in O(n log(n)) time
+using a binary search tree.'''
+
 tree_1 = BinarySearchTree(None)
 
 for name_1 in names_1:
@@ -26,3 +41,6 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# This runs in 0.0029 seconds on my machine.
+duplicates = list(set(names_1).intersection(names_2))
