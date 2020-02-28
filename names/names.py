ed0bf0c9b6c3e1e2ce868285452b8b9ec298be38
Aaron Clark <aaronpclark92@gmail.com>
2020-02-28 12:19:32 -0600

    added set intersection method for names stretch

--- names/names.py
+++ names/names.py
@@ -24,24 +24,35 @@ while name_idx <= len(names_1[1:]):
     names_1_tree.insert(names_1[name_idx])
     name_idx += 1
 
-# Replace the nested for loops below with your improvements
+# # Replace the nested for loops below with your improvements
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
 # * use list comp to create list of matching names
-# * run a binary search conains method on the names BST for every name in the *second* name list
+# * run a binary search conains method on the names BST for every name in the * second * name list
 duplicates = [name for name in names_2 if names_1_tree.contains(name)]
 
-# print(duplicates)
+# * solves in just under .05 seconds.
 
+
+def record_intersections(l1, l2):
+    intersections = [set(l1).intersection(l2)]
+    return intersections
+    # ** runs in .0040 seconds if l1 is a set and l2 is not a set, adds ~.01 seconds to convert both to set
+
+
+second_list = record_intersections(names_1, names_2)
 
 end_time = time.time()
-print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(second_list)
 print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# * intersection method
