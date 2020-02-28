93d95b1341f24c1927a74146899d61359762fc93
Jake <jlconnerly@gmail.com>
2020-02-28 12:34:25 -0600

    added binary_search_tree.py

---
+++ names/binary_search_tree.py
@@ -0,0 +1,41 @@
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+ 
+    def insert(self, value):
+        if value >= self.value:
+            if self.right == None:
+                self.right = BinarySearchTree(value)
+            else:
+                return self.right.insert(value)
+        elif value < self.value:
+            if self.left == None:
+                self.left = BinarySearchTree(value)
+            else:
+                return self.left.insert(value)
+
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif target > self.value:
+            if self.right != None:
+                return self.right.contains(target)
+        elif target < self.value:
+            if self.left != None:
+                return self.left.contains(target)
+        else:
+            return False
+
+    def get_max(self):
+        if self.right == None:
+            return self.value
+        return self.right.get_max()
+
+    def for_each(self, cb):
+        cb(self.value)
+        if self.right:
+            self.right.for_each(cb)
+        if self.left:
+            self.left.for_each(cb)
