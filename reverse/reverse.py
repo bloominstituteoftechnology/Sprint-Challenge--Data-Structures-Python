0d8f63ee383a53b7ab47d4e65bba9752b425810f
GCGsauce <gattigcg1@gmail.com>
2020-03-27 12:14:38 -0400

    done with MVP

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -45,6 +45,11 @@ class LinkedList:
         # if we've gotten here, then the target node isn't in our list
         return False
 
-    def reverse_list(self, node, prev):
+    def reverse_list(self, node, prev = None):
         # You must use recursion for this solution
-        pass
+        if node != None:
+            temp = node.next_node
+            node.next_node = prev
+            self.reverse_list(temp, node)
+        else:
+            self.head = prev
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,20 +1,32 @@
 from doubly_linked_list import DoublyLinkedList
 
-
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
+        self.oldest = None
 
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            if self.oldest == None:
+                self.oldest = self.storage.tail
+            self.oldest.value = item
+            self.oldest = self.oldest.prev
+        else:
+            self.storage.add_to_head(item)
+            if len(self.storage) == self.capacity:
+                self.oldest = self.storage.tail
+            
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        node = self.storage.tail 
+        while node != None:
+            list_buffer_contents += [node.value]
+            node = node.prev
 
         return list_buffer_contents
 
