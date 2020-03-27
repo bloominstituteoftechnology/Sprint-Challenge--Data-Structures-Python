0e0761df2a403d0f4ad864256b8d45a468600459
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2020-03-27 09:56:54 -0600

    Update ring_buffer.py
    investigating ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,14 +1,27 @@
 from doubly_linked_list import DoublyLinkedList
 
+# You want to define a buffer with a fixed size, 
+# so that when it fills up, adding another element must overwrite the first (oldest) one. 
+# This kind of data structure is particularly 
+# useful for storing log and history information. 
 
+# A ring buffer is a non-growable buffer with a fixed size. 
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
+        
 
+# When the ring buffer is full and a new element is inserted, 
+# the oldest element in the ring buffer is overwritten with the newest element.
     def append(self, item):
-        pass
+        """append an element at the end of the buffer"""
+        self.storage.add_to_tail(item)
+        if self.storage.length ==self.capacity:
+            self.current = 0
+
+            
 
     def get(self):
         # Note:  This is the only [] allowed
