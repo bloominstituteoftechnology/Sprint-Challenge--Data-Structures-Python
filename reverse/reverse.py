c5085ac88e7c2864e6bf1162108006dec9be127a
Marlon <marmoneymar@icloud.com>
2020-03-27 13:56:44 -0400

    Working on reverse. Taking too long, so I moved on.

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -47,4 +47,9 @@ class LinkedList:
 
     def reverse_list(self, node, prev):
         # You must use recursion for this solution
+        if node.get_next() == None:
+            self.head = node
+            return
+        self.reverse_list(node.get_next(), node)
+        
         pass
