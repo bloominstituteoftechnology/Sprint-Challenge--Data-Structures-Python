63cc6daa47ebe7a3d5f93c99d0f3448b2a64daeb
qweliant <qweliantanner@gmail.com>
2020-03-27 13:49:46 -0400

    cannot do circ buffer with dll

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -3,19 +3,39 @@ from doubly_linked_list import DoublyLinkedList
 
 class RingBuffer:
     def __init__(self, capacity):
-        self.capacity = capacity
-        self.current = None
+
         self.storage = DoublyLinkedList()
+        self.capacity = capacity
+        self.current_node = None
 
     def append(self, item):
-        pass
+
+        # normal insert 
+        if len(self.storage) < self.capacity:
+            self.storage.add_to_tail(item)
+            self.current_node = self.storage.tail
+
+        
+        else:
+
+            if self.current_node == self.storage.tail:
+                self.storage.remove_from_head()
+                self.storage.add_to_head(item)
+                self.current_node = self.storage.head
+            else:
+                self.storage.insert_after(self.current_node, item)
+                self.current_node = self.current_node.next
+                self.storage.delete(self.current_node.next)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
-
+        node = self.storage.head
+        while node:
+            list_buffer_contents.append(node.value)
+            node = node.next
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
