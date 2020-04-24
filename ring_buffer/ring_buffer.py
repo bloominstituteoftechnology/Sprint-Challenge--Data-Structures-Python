4582f9d844f6ae3d42853efb027b811df02eec17
cappers86 <52895127+cappers86@users.noreply.github.com>
2020-04-24 10:59:00 +0100

    ring buffer done getting one error from the stretch

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,32 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity: ## if the storage has capacity to add more 
+            self.storage.add_to_tail(item)## add to end tail
+            
+        else:
+            if self.current == None or self.current.next == None: ## if the current node is the last//no space
+                self.current = self.storage.head
+            
+    ##Wrap the given value in a ListNode and insert it
+    ##after this node. Note that this node could already
+    ##have a next node it is point to.
+            else: ## current to next 
+                self.current = self.current.next
+            
+            self.current.value = item
+
+
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        node = self.storage.head
+        while node != None:
+            list_buffer_contents.append(node.value)
+            node = node.next
+
 
         return list_buffer_contents
 
