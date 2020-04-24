38dc515762de26a2a582129a7116c02169738e1d
Claudia Chajon <53544970+claudiasofiaC@users.noreply.github.com>
2020-04-24 12:46:21 -0400

    names and bst

---
+++ names.py
@@ -0,0 +1,45 @@
+import time
+from binary_tree_search import BinarySearchTreeNode
+
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates = []  # Return the list of duplicates in this data structure
+
+# Replace the nested for loops below with your improvements
+#for name_1 in names_1:
+#    for name_2 in names_2:
+#        if name_1 == name_2:
+#            duplicates.append(name_1)
+
+
+bst = BinarySearchTreeNode(names_1[0])
+# use names_1 in the bst
+# then use contain of the bst to see if names in names_2 are dups
+# append to dup list if they are dups
+
+for name in names_1:
+    bst.insert(name)
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
+
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
+# runtime: 0.13433384895324707 seconds
+
+
+# ---------- Stretch Goal -----------
+# Python has built-in tools that allow for a very efficient approach to this problem
+# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# structures, but you may not import any additional libraries that you did not write yourself.
