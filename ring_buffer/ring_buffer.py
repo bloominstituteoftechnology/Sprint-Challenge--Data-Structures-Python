6dff253963aebfac8961609e8b6e6d7942cbeea4
Claudia Chajon <53544970+claudiasofiaC@users.noreply.github.com>
2020-04-24 12:49:40 -0400

    ring_buffer_tested

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,13 +8,49 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        # head is LRU
+
+        # if list is empty
+        if self.current == None:
+            self.storage.add_to_tail(item)
+            cur = self.storage.head
+            self.current = cur.next
+
+        # if list is full/ no empty spaces
+        elif len(self.storage) == self.capacity:
+
+            # replace value
+            self.current.value = item
+
+            if self.current == self.storage.tail:
+                self.current = self.storage.head
+            else:
+                self.current = self.current.next
+
+        # list is not completely full
+        else:
+            self.storage.add_to_tail(item)
+            if len(self.storage) == self.capacity:
+                self.current = self.storage.head
+
+
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
+        # cursor
+        cur = self.storage.head
+
+        # iterate through dll
+        while cur.next:
+            # if not None, put it in the list
+            if cur.next:
+                list_buffer_contents.append(cur.value)
+            cur = cur.next
+
+        # add last value
+        list_buffer_contents.append(cur.value)
 
         return list_buffer_contents
 
