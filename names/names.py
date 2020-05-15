aa175f7cced6f7e27775661da54b4f2700506f97
Alexis <alexisdavalos.tech@gmail.com>
2020-05-15 14:14:40 -0400

    names.py stretch

--- names/names.py
+++ names/names.py
@@ -16,10 +16,15 @@ for name in names_1:
     #O(log(n))
     bts_1.insert(name)
 
+#Stretch
+# set1 = set(names_1)
+
 f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+#Stretch
+# set2 = set(names_2)
 
 duplicates = []  # Return the list of duplicates in this data structure
 
@@ -38,6 +43,9 @@ for name in names_2:
         # adds name to duplicates list if returns True
         duplicates.append(name)
 
+#Stretch
+# duplicates = set1.intersection(set2)
+
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
@@ -46,3 +54,7 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# By using built in intersection method on sets I was able to bring the run time
+# Down from 0.38s to 0.0129s. I was lucky enough to stumble upon this method by googling intersection python method
+# instead of using btree or O(n^2) nested loop original solution, this is much faster. Wasn't expecting such a huge difference
