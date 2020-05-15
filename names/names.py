4dda1e154505bb038ec2ba31e17fba368f7874ca
Arash Haji-Hassanzadeh <56742302+arashhaji@users.noreply.github.com>
2020-05-15 03:35:12 -0400

    names
    original runtime: 6.412154197692871
    
    new runtime: 0.10668492317199707
    
    streth runtime:0.004072904586791992

--- names/names.py
+++ names/names.py
@@ -1,4 +1,7 @@
 import time
+from binary import BSTNode 
+# from collections import deque
+
 
 start_time = time.time()
 
@@ -13,10 +16,53 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# O(n^n) for loop within a four loop
+
+# O(n log n) due to bigger list its log n
+
+# binary search tree for the list
+
+# original runtime: 6.412154197692871 
+
+binary = BSTNode(names_1[0]) #runtime: 0.10668492317199707 seconds
+
+for name in names_1: # checks if name is in list 
+    binary.insert(name)
+
+for name in names_2: #checks if name is in list 
+    if binary.contains(name): 
+        duplicates.append(name) # appends duplicates
+
+
+#stretch
+
+# collections = deque(names_1[0])
+
+# for name in names_1:
+#     collections.append(name)
+
+# for name in names_2:
+#     if collections.contains(name):
+#         duplicates.append(name)
+
+# set_1 = set(names_1)                        # runtime: 0.004072904586791992 seconds
+# set_2 = set(names_2)
+
+# duplicates = set_1.intersection(set_2)
+
+# cast each input list to a set
+# a set in Python is an unordered collection with no duplicate elements
+# sets have an intersection method to return unique elements found in both sets  
+# intersection_set = set1.intersection(set2)
+# cast intersection_set as list
+# return list
+# https://dfrieds.com/python/intersection-two-arrays.html
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,25 +1,25 @@
-class RingBuffer: # a buffer with a fixed size, so that when it fills up, adding another element must overwrite the first (oldest) one its useful for storing log and history info 
-    def __init__(self, capacity): # sets the capacity to non-full buffer 
+class RingBuffer: # a buffer with a dynamic size, so that when it fills up, adding another element must overwrite the first (oldest) one its useful for storing log and history info 
+    def __init__(self, capacity): # sets the capacity to an empty list 
         self.capacity  = capacity 
         self.storage = []
         
     def append(self, item): 
-        self.storage.append(item) # append an element at the end of the buffer
+        self.storage.append(item) # append an element at the end of the list
         if len(self.storage) == self.capacity:
             self.cur = 0
             self.__class__ = self.__Full
     
     def get(self): # returns a list of items from the oldest to newest 
             return self.storage
-    class __Full: #class that implements a full buffer 
+    class __Full: #checking the length of capacity in the list at its maximum 
         def __init__(self, n):
-            raise
+            raise 
 
-        def append(self, x): 
+        def append(self, x): # add a node for the newest value and removes the newest node 
             self.storage[self.cur] = x
             self.cur = (self.cur+1) % self.capacity
 
-        def get(self): #return list of elements in correct order
+        def get(self): #returns a list of items from the oldest to newest 
             return self.storage
 
 # Reference
