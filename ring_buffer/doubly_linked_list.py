f453e52afd97996c5fc117d8f6f1962e8abea90c
JFerrer95 <jonathanferrer95@gmail.com>
2020-05-15 11:52:35 -0400

    ring buffer passing tests

---
+++ ring_buffer/doubly_linked_list.py
@@ -0,0 +1,105 @@
+class ListNode:
+    def __init__(self, value, prev=None, next=None):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+    def insert_after(self, value):
+        current_next = self.next
+        self.next = ListNode(value, self, current_next)
+        if current_next:
+            current_next.prev = self.next
+
+    def insert_before(self, value):
+        current_prev = self.prev
+        self.prev = ListNode(value, current_prev, self)
+        if current_prev:
+            current_prev.next = self.prev
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
+
+class DoublyLinkedList:
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
+
+    def __len__(self):
+        return self.length
+
+    def add_to_head(self, value):
+        self.length += 1
+        new_node = ListNode(value)
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+    def remove_from_head(self):
+
+        value = self.head.value
+        self.delete(self.head)
+        return value
+        
+    def add_to_tail(self, value):
+        new_node = ListNode(value, None, None)
+        self.length += 1
+        #  If the list is empty
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def remove_from_tail(self):
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
+
+    def move_to_front(self, node):
+        if node is self.head:
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
+
+    def move_to_end(self, node):
+        if node is self.tail:
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
+
+
+    def delete(self, node):
+        self.length -= 1
+
+        if self.head is self.tail:
+            self.head = None
+            self.tail = None
+
+        elif node is self.head:
+            self.head = node.next
+            node.delete()
+        elif node is self.tail:
+            self.tail = node.prev
+            node.delete()
+        else:
+            node.delete()
+            
+    def get_max(self):
+        value = self.head.value
+        current = self.head
+
+        while current is not None:
+            if current.value > value:
+                value = current.value
+            current = current.next
+        return value
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,34 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.oldest = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        
+        if self.capacity > len(self.storage):
+            self.storage.add_to_tail(item)
+            if len(self.storage) == 1:
+                self.oldest = self.storage.head
+        
+        else:
+            self.oldest.value = item
+
+            if self.oldest is not self.storage.tail:
+                self.oldest = self.oldest.next
+            else:
+                self.oldest = self.storage.head
+
+        
 
     def get(self):
-        pass
+        ring_buffer_list = []
+        node = self.storage.head
+        while node is not None:
+            ring_buffer_list.append(node.value)
+            node = node.next
+
+        return ring_buffer_list
