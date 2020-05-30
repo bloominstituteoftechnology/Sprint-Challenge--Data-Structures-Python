b0de1182707bdeabbc3503c1245c7e1df79710d7
Michael Stoffer <mstoffer@michaelstoffer.com>
2020-05-30 13:15:35 -0400

    Completed Runtime Optimization

--- names/names.py
+++ names/names.py
@@ -12,11 +12,14 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+name_dict = {}
+
+for val in names_1:
+	name_dict[val] = True
+
+for val in names_2:
+	if val in name_dict:
+		duplicates.append(val)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
