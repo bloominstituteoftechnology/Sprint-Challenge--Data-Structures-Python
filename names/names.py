16fc33319e164f37ee73bd3fcb12a12d0c4018a1
unknown <dj.zaragoza@icloud.com>
2020-05-30 10:54:52 -0700

    completed stretch goal for names.py to improve runtime by .1s

--- names/names.py
+++ names/names.py
@@ -41,3 +41,6 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  There are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+print(f"{len(set(names_1) & set(names_2))} duplicates using built-in-tool set: {set(names_1) & set(names_2)}")
+print (f"runtime: {end_time - start_time} seconds, which is much faster compared to the previous code's runtime: 0.1678149700164795 seconds, and the original code's runtime: 11.417479038238525 seconds")
