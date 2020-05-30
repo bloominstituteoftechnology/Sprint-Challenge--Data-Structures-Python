0f617ab516ab018163587816d303ea1c0bef8d66
newnativeabq <vincent.a.brandon@gmail.com>
2020-05-30 11:40:57 -0600

    ENH: Completed ring buffer.  Passing tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,35 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity 
+        self.store = []
+        self.index = -1
 
     def append(self, item):
-        pass
+        if item is not None:
+            # Fill buffer
+            if len(self.store) < self.capacity:
+                self.store.append(item)
+            # Re-write buffer in order
+            else:
+                index = self._get_active()
+                # print(index)  # DEBUG
+                self.store[index] = item
+
+    def _get_active(self):
+        if self.index < self.capacity - 1:
+            self.index += 1
+            return self.index
+        else:
+            self.index = 0
+            return self.index
 
     def get(self):
-        pass
+        return self.store
+
+
+
+if __name__ == "__main__":
+    buffer = RingBuffer(3)
+    for item in ['a', 'b', 'c', 'd', 'e', 'f', 'g']:
+        buffer.append(item)
+        print(buffer.get())
