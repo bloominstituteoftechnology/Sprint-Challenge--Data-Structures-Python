b72cde0ada776e245c341dc60e1c892c3a61f2f0
unknown <dj.zaragoza@icloud.com>
2020-05-30 09:16:48 -0700

    finished ring_buffer file implementing a ring buffer data structure

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
+        self.capacity = capacity
+        self.storage = []
+        self.current_node = 0
         pass
 
     def append(self, item):
+        if len(self.storage) == self.capacity:
+            self.storage.pop(self.current_node)
+            self.storage.insert(self.current_node, item)
+            self.current_node = (self.current_node + 1) % self.capacity
+        else:
+            self.storage.append(item)
         pass
 
     def get(self):
+        return self.storage
         pass
