c9e2cfde831808bfdd36e259b7a739bbc7233912
AbreuOliver <oliverabreu@icloud.com>
2020-05-30 15:56:26 -0400

    This commit should record solution to 'reverse.py'

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,13 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        current_position = self.head # START AT THE HEAD OF THE LINKED LIST
+        next_node = None # SET THE VALUE OF 'NEXT' TO 'None' TO START
+        prev = None # THERE IS NO 'PREVIOUS' TO POINT TO, YET 
+        
+        while current_position is not None:
+            next_node = current_position.next_node # POINT THE VALUE OF THE NEXT NODE TO BE VALUE OF THE NEXT NODE
+            current_position.next_node = prev # REVERSE THE POINTER FOR NEXT TO BE PREVIOUS  
+            prev = current_position # CHANGE POINTER FROM 'PREVIOUS' TO 'CURRENT'
+            current_position = next_node # CHANGE POINTER FROM 'CURRENT' TO 'NEXT'
+        self.head = prev
