2dd1a94eb3f7b1087ec6c0eae801203092fa2442
newnativeabq <vincent.a.brandon@gmail.com>
2020-05-30 11:56:29 -0600

    ENH: updated test_reverse to use reverse list.  no args required besides implicit self

--- names/names.py
+++ names/names.py
@@ -10,19 +10,74 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
-
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
-end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+def answer_1(names_1, names_2):
+    # ~ 4x faster
+    duplicates = []
+    for name in names_1:
+        if name in names_2:
+            duplicates.append(name)
+    return duplicates
+
+
+class HashMap():
+    def __init__(self):
+        self.storage = {}
+
+    def create_set(self, items):
+        i = 0
+        for item in items:
+            self.storage[item] = i
+            i += 1
+
+    def add_items(self, items):
+        i = len(self.storage)
+        duplicates = []
+        for item in items:
+            if item in self.storage:
+                duplicates.append(item)
+            else:
+                self.storage[item] = i
+                i += 1
+        return duplicates
+
+
+def answer_1b(names_1, names_2):
+    map_1 = HashMap()
+    map_1.create_set(names_1)
+    duplicates = map_1.add_items((names_2))
+    return duplicates
+
+
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+
+
+# Python has a data structure called Set that can do these comparisons via intersection (time = 0.003s)
+
+def answer_2(names_1, names_2):
+    duplicates = set(names_1).intersection(set(names_2))
+    return duplicates
+
+def run_test(func, names_1, names_2):
+    print('Running: ', func)
+    start_time = time.time()
+    duplicates = func(names_1, names_2)
+    end_time = time.time()
+    print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+    print (f"runtime: {end_time - start_time} seconds")
+
+if __name__ == "__main__":
+    run_test(answer_1, names_1, names_2)
+    run_test(answer_1b, names_1, names_2)
+    run_test(answer_2, names_1, names_2)
--- reverse/reverse.py
+++ reverse/reverse.py
@@ -1,42 +1,64 @@
 class Node:
-    def __init__(self, value=None, next_node=None):
-        self.value = value
-        self.next_node = next_node
+  def __init__(self, value=None, next_node=None):
+    # the value at this linked list node
+    self.value = value
+    # reference to the next node in the list
+    self.next_node = next_node
 
-    def get_value(self):
-        return self.value
+  def get_value(self):
+    return self.value
 
-    def get_next(self):
-        return self.next_node
+  def get_next(self):
+    return self.next_node
 
-    def set_next(self, new_next):
-        self.next_node = new_next
+  def set_next(self, new_next):
+    # set this node's next_node reference to the passed in node
+    self.next_node = new_next
 
 class LinkedList:
-    def __init__(self):
-        self.head = None
+  def __init__(self):
+    # reference to the head of the list
+    self.head = None
 
-    def add_to_head(self, value):
-        node = Node(value)
+  def add_to_head(self, value):
+    node = Node(value)
+    if self.head is not None:
+      node.set_next(self.head)
+    
+    self.head = node
 
-        if self.head is not None:
-            node.set_next(self.head)
+  def contains(self, value):
+    if not self.head:
+      return False
+    # get a reference to the node we're currently at; update this as we traverse the list
+    current = self.head
+    # check to see if we're at a valid node 
+    while current:
+      # return True if the current value we're looking at matches our target value
+      if current.get_value() == value:
+        return True
+      # update our current node to the current node's next node
+      current = current.get_next()
+    # if we've gotten here, then the target node isn't in our list
+    return False
 
-        self.head = node
-
-    def contains(self, value):
-        if not self.head:
-            return False
-
-        current = self.head
-
-        while current:
-            if current.get_value() == value:
-                return True
-
-            current = current.get_next()
-
-        return False
-
-    def reverse_list(self, node, prev):
-        pass
+  def reverse_list(self):
+    stack = []
+    node = self.head
+    # Two steps for this implementation.
+    #   1. Save out everything into a stack (could save out connections vs values, but since values are so small...)
+    #   2. Rewrite all values
+    if node is not None:
+      while True:
+        stack.append(node.value)
+        if node.next_node is not None:
+          node = node.next_node
+        else:
+          break
+      node = self.head
+      while True:
+        node.value = stack.pop()
+        if node.next_node is not None:
+          node = node.next_node
+        else:
+          break
