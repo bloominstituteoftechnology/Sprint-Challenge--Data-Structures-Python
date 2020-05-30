dae2917550da8ed0f61297bdfae872d28711fbef
kly001 <karenly1961@gmail.com>
2020-05-29 21:31:10 -0400

    ring_buffer.py: 3 tests passing so far

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,15 @@
 class RingBuffer:
+    #  define initial setting of the RingBuffer class
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        self.current = 0
 
     def append(self, item):
-        pass
+        self.data.append(item)
+        if len(self.data) == self.capacity:
+            self.current = 0
 
     def get(self):
-        pass
+        # return a list of items from the oldest to the newest
+        return self.data
