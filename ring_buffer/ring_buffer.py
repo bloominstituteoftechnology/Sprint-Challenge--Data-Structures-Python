db1b489d4eea02174c01548c050b508f63b0afc3
Tevvin <Tevvin7395@icloud.com>
2020-05-30 14:14:29 -0400

    Change of plans this works better

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,16 +1,14 @@
-# Node Class
-class Node:
-    def __init__(self, item):
-        self.item = item
-        self.next = None
-
-
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
+    def __init__(self, capacity):  # Default
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
 
-    def append(self, item):
-        pass
+    def append(self, item):  # overwrites oldest node
+        self.storage[self.current] = item
+        self.current += 1
+        if self.current == self.capacity:
+            self.current = 0
 
-    def get(self):
-        pass
+    def get(self):  # returns new values after append
+        return [x for x in self.storage if x is not None]
