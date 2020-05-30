76214894f79076c5102880c8ca09e5f772b97600
kly001 <karenly1961@gmail.com>
2020-05-29 21:49:49 -0400

    names.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,10 +6,20 @@ class RingBuffer:
         self.current = 0
 
     def append(self, item):
-        self.data.append(item)
-        if len(self.data) == self.capacity:
-            self.current = 0
+        # if the current length of the list is less than the capacity, 
+        # append an element at the end of the buffer
+        if len(self.data) < self.capacity: 
+            self.data.append(item)
+        else:
+            self.current += 1
+            if self.current > (self.capacity) - 1:
+                self.current = 0
 
     def get(self):
         # return a list of items from the oldest to the newest
         return self.data
+
+
+
+        ##  Sources:
+            # 1.  http://code.activestate.com/recipes/68429-ring-buffer/
