71046fcedc7ce70b53cf660e7490d3f436546dc5
Denvinn Magsino <denvinnmagsino@Denvinns-MacBook-Air.local>
2020-05-31 16:00:59 -0700

    finished ring_biffer: All test OK

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,41 @@
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
+    def __init__(self, capacity):   
+        self.capacity = capacity
+        self.storage = []
+        self.tracker = 0
+        
 
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            self.storage[self.tracker] = item
+            if self.tracker + 1 == self.capacity:
+                self.tracker = 0
+            else: 
+                self.tracker += 1
+        else: 
+            self.storage.append(item)
+        return item
 
     def get(self):
-        pass
+        return self.storage
+
+rb = RingBuffer(4)
+
+rb.append(1)
+rb.append(2)
+rb.append(3)
+rb.append(1)
+rb.append(5)
+rb.append(7)
+rb.append(89)
+rb.append(99)
+rb.append(20)
+rb.append(2)
+rb.append(3)
+rb.append(1)
+
+print(rb.get())
+
+
+
+
