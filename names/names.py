89d12364ac4d23fd1e48adaac5e2253f74f68c0e
kiamco <kiamcoo@gmail.com>
2020-05-31 19:02:39 -0700

    completed

--- names/names.py
+++ names/names.py
@@ -10,13 +10,17 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+# you can also do [x for x in names_1 if x in names_2]
+# *set(names_1).intersection(names_2)
+duplicates = [*set(names_1).intersection(names_2)]  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# # Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -26,3 +30,15 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# duplicate2 = [x for x in names_1 if x in names_2] #1.3 seconds
+
+
+# hash method O(n) takes ~0.01 to complete
+# dict = {}
+# for i in names_1:
+#     dict[i] = True
+    
+# for x in names_2:
+#     if x in dict:
+#         duplicates.append(x)
--- reverse/reverse.py
+++ reverse/reverse.py
@@ -11,6 +11,9 @@ class Node:
 
     def set_next(self, new_next):
         self.next_node = new_next
+        
+    def __str__(self):
+        return f'{self.value}'
 
 class LinkedList:
     def __init__(self):
@@ -37,6 +40,39 @@ class LinkedList:
             current = current.get_next()
 
         return False
+    
+    def traverse(self):
+        current = self.head
+        
+        while current:
+            print(current)
+            current = current.next_node
 
     def reverse_list(self, node, prev):
-        pass
+        pointer = self.head
+
+        while pointer:
+        # hold next node 
+            next = pointer.next_node
+
+        # delete the current pointer.next
+            pointer.next_node =  prev
+
+        #assing prev to the current pointer
+            prev = pointer
+
+        # step to next node
+            pointer = next
+        
+        self.head = prev
+
+
+# list = LinkedList()
+# list.add_to_head(1)
+# list.add_to_head(2)
+# list.add_to_head(3)
+# list.add_to_head(4)
+# list.add_to_head(5)
+# list.add_to_head(6)
+# list.reverse_list(list.head, None)
+# list.traverse()
