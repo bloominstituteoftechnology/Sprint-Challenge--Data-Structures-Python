8a78d226e4d11d83e53fce600c698c9da258ed8f
Mayankho <yankhot96@gmail.com>
2020-06-01 19:39:04 -0400

    All code on local machine MVP

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,41 @@
+from DLL import DoublyLinkedList
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current_node = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        #Will see is the size of storage is less than expected
+
+        if len(self.storage) < self.capacity: 
+            #set current_node to storage head
+            #add to tail to prevent checking for head
+            self.storage.add_to_tail
+            self.current_node = self.storage.head
+        else: #Just in case capacity is reached
+            #Does the current node have a next?
+            if not self.current_node.next:
+                self.current_node.value = item
+                self.current_node = self.storage.head
+            else: 
+
+                self.current_node.value = item
+                self.current_node = self.current_node.next
+            
+        
 
     def get(self):
-        pass
+        # Make an empty array
+        arr = []
+        #Create a current variable and set it to head
+        current = self.storage.head
+        while current: #While there is still a current value, we append to the array
+            arr.append(current.value)
+            
+            current = current.next
+        return arr
+            
+
+    
