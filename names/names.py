a062e3d1918dfc80fad95aec9a914ffc09256ca5
tgc <codingviewdmv@gmail.com>
2020-06-12 13:26:52 -0400

    increased speed on stretch names method redue

--- names/names.py
+++ names/names.py
@@ -38,11 +38,11 @@ import time
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
+names_1 = f.read().split("\n")  
 f.close()
 
 f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
+names_2 = f.read().split("\n") 
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
@@ -64,3 +64,24 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+start_time = time.time()
+
+file = open('names_1.txt', 'r')
+names_1 = file.read().split("\n") 
+file.close()
+
+file = open('names_2.txt', 'r')
+names_2 = file.read().split("\n") 
+file.close()
+
+dupes = []
+names_set = set(names_1) #reArranes iterables in order, an some how increases speed by 1.0322
+
+for name in names_2:
+    if name in names_set:
+        dupes.append(name)
+
+end_time = time.time()
+print (f"{len(dupes)} duplicates:\n\n{', '.join(dupes)}\n\n")
+print (f"fastest runtime: {end_time - start_time} seconds")
