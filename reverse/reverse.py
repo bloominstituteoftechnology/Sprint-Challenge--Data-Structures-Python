9e4734bd5342837d081c6ffac6c2e9818ad08100
Mark Gerrior <github@gerrior.com>
2020-06-12 10:06:41 -0700

    reverse_list complete

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,16 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if node:
+            # tuple swap
+            # prev becomes then next_node
+            # then next_node becomes prev
+            # 1st time through: node (1) pointing to 2    and prev to None.                 Code changes Prev to 2    and node (1) pointing to None.
+            # 2nd time through: node (2) pointing to 3    and prev to 1 (pointing to None). Code changes Prev to 3    and node (2) pointing to 1. 
+            # 3rd time through: node (3) pointing to None and prev to 2 (pointing to 1   ). Code changes Prev to None and node (3) pointing to 2. 
+            prev, node.next_node = node.next_node, prev
+
+        if prev: # If prev is not None then we have another element to swap
+            self.reverse_list(prev, node)
+        else: # We'll know we've gotten to the end when prev is None so set head to this node
+            self.head = node
