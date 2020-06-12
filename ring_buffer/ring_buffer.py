a82c4651dccce2f38e31e3f7a9d3ac0a51c2f630
CC <ciarabeitel@gmail.com>
2020-06-12 11:40:01 -0400

    Complete Ring Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = [] # start with an empty list
+        self.oldestItem = 0 # declare the oldest item is at the 0 index
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity: # if there is some vacancies
+            self.storage.append(item) # book a room
+        else:
+            self.storage[self.oldestItem] = item # if there are no vacancies, kick the first tenants out and replace with item
+            if self.oldestItem < len(self.storage) - 1: # check to see who is the oldest
+                self.oldestItem += 1 # move down the line
+            else: #otherwise 
+                self.oldestItem = 0 # kick the first tenant in the list out
 
     def get(self):
-        pass
+        return self.storage
