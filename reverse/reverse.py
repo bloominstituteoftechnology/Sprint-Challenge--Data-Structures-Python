6c66611fe748bb1f319192a6d3a7a6e0430f772e
Lydia Zhang <art4everyan@gmail.com>
2020-06-12 12:01:34 -0400

    reverse

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,11 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        current = node
+
+        while current is not None:
+            next = current.next_node
+            current.next_node = prev
+            prev = current
+            current = next
+        self.head = prev
