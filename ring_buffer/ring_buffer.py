6dd78197c1e27de82094c4df18b27d00df2a4fb5
Mark Gerrior <github@gerrior.com>
2020-06-12 08:58:40 -0700

    Ring Buffer Complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,56 +1,19 @@
-class Node: 
-    def __init__(self, value = None, next_node = None ):
-        self.value = value
-        self.next_node = next_node 
-
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.size = 0
-        self.first_node = None
-        self.last_node = None
+        self.storage = []
+        self.ring_position = -1
 
     def append(self, item):
-        if self.size == self.capacity:
-            self.pop()
+        self.ring_position += 1
 
-        self.size += 1
+        if self.ring_position >= self.capacity:
+            self.ring_position = 0
 
-        new_node = Node(item)
-
-        if self.first_node == None:
-            # First element 
-            self.first_node = new_node
-            self.last_node = new_node
-            return 
-
-        self.last_node.next_node = new_node
-        self.last_node = new_node
-
-    def pop(self):
-        if self.size == 0:
-            return None
-
-        remove_first_node = self.first_node
-        self.first_node = remove_first_node.next_node
-        remove_first_node.next_node = None
-
-        self.size -= 1
-
-        if self.size == 0:
-            self.first_node = None
-            self.last_node = None
-
-        return remove_first_node.value
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        else:
+            self.storage[self.ring_position] = item
 
     def get(self):
-        current_item = self.first_node
-        entire_buffer = []
-        while True:
-            entire_buffer.append(current_item.value)
-            if current_item == self.last_node:
-                break
-            current_item = current_item.next_node
-
-        return entire_buffer
-
+        return self.storage
