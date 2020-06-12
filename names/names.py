1faa0b138195c0a8c4ed0f3031d1e009142335c6
Mark Gerrior <github@gerrior.com>
2020-06-12 10:43:31 -0700

    My second names solution

--- names/names.py
+++ names/names.py
@@ -24,11 +24,55 @@ duplicates = []  # Return the list of duplicates in this data structure
 # duplicates = [name for name in names_2 if name in set_of_names_1]
 # runtime: 0.005300045013427734 seconds
 
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
+    # Insert the given value into the tree
+    def insert(self, value):
+        if value >= self.value:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
+        else:
+            if self.left:
+                self.left.insert(value)
+            else: 
+                self.left = BSTNode(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if target == self.value:
+            return True 
+        else:
+            if target >= self.value:
+                if self.right:
+                    return self.right.contains(target)
+                else:
+                    return False
+            else: 
+                if self.left:
+                    return self.left.contains(target)
+                else:
+                    return False
+
+nameTree = BSTNode("N")
+for name in names_1:
+    nameTree.insert(name)
+
+for name in names_2:
+    if nameTree.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+# runtime: 0.1528627872467041 seconds
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
