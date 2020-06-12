112090b2f4944a88f542744700072ffb3feeee8c
Mario Rivera <mario@Marios-MacBook-Pro.local>
2020-06-12 08:48:20 -0700

    imported Doubly LL, testing APPEND

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
+from doubly_linked_list import DoublyLinkedList
+
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
+        self.current = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+        elif self.storage.length == self.capacity:
+            if not self.storage.tail.next:
+                self.storage.tail.next = self.storage.head
+                self.current = self.storage.head
+            self.current.value = item
+            self.current = self.current.next
 
     def get(self):
         pass
