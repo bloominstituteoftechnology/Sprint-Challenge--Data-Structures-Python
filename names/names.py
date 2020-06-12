543206b70b13d1da34538106fb52e0238b8b2dbc
Christopher DeVito <christopher.devito@protonmail.com>
2020-06-12 12:38:27 -0400

    Refactored names.py to O(n)

--- names/names.py
+++ names/names.py
@@ -1,5 +1,6 @@
 import time
 
+## original runtime complexity is O(n^c) and for frame of reference runs in 9.16 seconds on my computer
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -13,10 +14,15 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+for name in names_1:
+    if name in names_2:
+        duplicates.append(name)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
