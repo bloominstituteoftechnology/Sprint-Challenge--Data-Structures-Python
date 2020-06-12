83a9a326d69ec69eeab81409c1c9a5cb68efff8b
Jon Bash <jonbash@protonmail.com>
2020-06-12 08:18:11 -0700

    complete ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.__storage = [None] * capacity
+        self.__head = 0
 
     def append(self, item):
-        pass
+        self.__storage[self.__head] = item
+        self.__head = (self.__head + 1) % self.capacity
 
     def get(self):
-        pass
+        start = None
+        end = None
+        for i, x in enumerate(self.__storage):
+            if start is None and x is not None:
+                start = i
+            elif end is None and x is None:
+                end = i
+        return self.__storage[start:end]
