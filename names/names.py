00f81d222dce6655bfc284f3ff4afd8b9e0d3154
Jon Bash <jonbash@protonmail.com>
2020-06-12 09:25:37 -0700

    add stretch version to names.py

--- names/names.py
+++ names/names.py
@@ -13,12 +13,11 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-search_tree = BSTNode(names_1[0])
+bst = BSTNode(names_1[0])
 for name in names_1[1:]:
-    search_tree.insert(name)
+    bst.insert(name)
 for name in names_2:
-    if search_tree.contains(name):
+    if bst.contains(name):
         duplicates.append(name)
 
 end_time = time.time()
@@ -26,6 +25,26 @@ print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
-# Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
-# structures, but you may not import any additional libraries that you did not write yourself.
+# Python has built-in tools that allow for a very efficient approach to this
+# problem. What's the best time you can accomplish?  Thare are no restrictions
+# on techniques or data structures, but you may not import any additional
+# libraries that you did not write yourself.
+
+print("---STRETCH VERSION USING SETS---")
+
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = set(f.read().split("\n"))  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = set(f.read().split("\n"))  # List containing 10000 names
+f.close()
+
+# Return the list of duplicates in this data structure
+duplicates = list(names_1 & names_2)
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
