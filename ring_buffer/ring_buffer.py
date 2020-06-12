ea020e0b997629dc39844e06b02b1a0a14eda1e2
Wyatt Harrell <wyattaharrell@gmail.com>
2020-06-12 12:02:58 -0400

    ring buffer complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
+from collections import deque
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.items = list()[-capacity:]
+        self.index = 0
 
     def append(self, item):
-        pass
+        if len(self.items) == self.capacity:
+            self.items[self.index] = item
+        else:
+            self.items.append(item)
+        self.index = (self.index + 1) % self.capacity
+
 
     def get(self):
-        pass
+        return self.items
