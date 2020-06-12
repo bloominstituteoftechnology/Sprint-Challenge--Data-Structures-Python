844e5c90e0c11113fb8aa8038eae8515fbb39a3a
Hector <hectoremil21@hotmail.com>
2020-06-12 11:21:37 -0400

    Implement append

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.stack = []
 
     def append(self, item):
-        pass
+        # Check if we're not past our capacity
+        if len(self.stack) <= self.capacity:
+            # Check if item isn't already on the list:
+            if item not in self.stack:
+                self.stack.append(item)
+            # If item is in list, pop it out of the list and append returned value
+            else:
+                self.stack.append(self.stack.pop(item))
+        # If we're past capacity, delete index 0 and call append again
+        else:
+            self.stack.pop(0)
+            self.append(item)
 
     def get(self):
         pass
