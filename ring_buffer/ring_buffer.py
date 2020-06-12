ea236b2715948b136cd261b63ca0fca0010bea9b
Mario Rivera <mario@Marios-MacBook-Pro.local>
2020-06-12 09:04:59 -0700

    ring buffer working and tested

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -20,4 +20,11 @@ class RingBuffer:
         list_buffer_contents = []
         list_buffer_contents.append(self.storage.head.value)
         next_node = self.storage.head.next
-        
+        for i in range(self.capacity - 1):
+            if next_node is None:
+                pass
+            else:
+                list_buffer_contents.append(next_node.value)
+                next_node = next_node.next
+
+        return list_buffer_contents
