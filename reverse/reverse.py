8d5ad6aed15fdad7cbd1b1c64d606b50bd9811e7
Pete1701 <pete.pertongonas@pirron.one>
2020-06-12 19:58:58 +0200

    reverse iterative solution

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -14,8 +14,7 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        self.head = None
-        # self.tail = None
+        self.head = None        
 
     def add_to_head(self, value):
         node = Node(value)
@@ -40,16 +39,31 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
+        # Recursive Solution
+        
         if self.head is None or self.head.next_node is None:
             return
+        if node.next_node is None:
+            self.head = node
+            self.head.next_node = prev
+            return node            
+
+        self.reverse_list(node.next_node, node)
+        node.next_node = prev
+
+
+        # Iterative Solution
+
+        # if self.head is None or self.head.next_node is None:
+        #     return
         
-        prev = None
-        current = self.head
+        # prev = None
+        # current = self.head
 
-        while current:
-            next_element = current.next_node
-            current.next_node = prev
-            prev = current
-            current = next_element
+        # while current:
+        #     next_element = current.next_node
+        #     current.next_node = prev
+        #     prev = current
+        #     current = next_element
 
-        self.head = prev
+        # self.head = prev
