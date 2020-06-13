b9e6a8f2a4edcf67778d90b3428e17468a787ed7
Hector <hectoremil21@hotmail.com>
2020-06-12 20:33:29 -0400

    Implement BST for names.py

--- names/names.py
+++ names/names.py
@@ -1,5 +1,113 @@
 import time
 
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        # compare the value to the root's value to determine which direction
+        # we're gonna go in
+        # if the value < root's value
+        if value < self.value:
+            # go left
+            if self.left:
+                # then self.left is a Node
+                self.left.insert(value)
+            else:
+                self.left = BSTNode(value)
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
+
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if target < self.value:
+            if self.left:
+                return self.left.contains(target)
+        elif target > self.value:
+            if self.right:
+                return self.right.contains(target)
+        return self.value == target
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        if self.right is None:
+            return self.value
+        else:
+            return self.right.get_max()
+
+    # Call the function `fn` on the value of each node
+    def for_each(self, fn):
+        fn(self.value)
+        if self.left is not None:
+            self.left.for_each(fn)
+        if self.right is not None:
+            self.right.for_each(fn)
+
+    # Part 2 -----------------------
+
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+    def in_order_print(self, node):
+        if node is None:
+            return
+        self.in_order_print(node.left)
+        print(node.value)
+        self.in_order_print(node.right)
+
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative breadth first traversal
+    def bft_print(self, node):
+        q = [node]
+        while True:
+            # print("---------")
+            val = q[0]
+            print(val.value)
+            q.remove(val)
+            if val.left:
+                q.append(val.left)
+            if val.right:
+                q.append(val.right)
+            if len(q) == 0: break
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative depth first traversal
+    def dft_print(self, node):
+        if node is None:
+            return
+        print(node.value)
+        self.dft_print(node.left)
+        self.dft_print(node.right)
+
+
+    # Stretch Goals -------------------------
+    # Note: Research may be required
+
+    # Print Pre-order recursive DFT
+    def pre_order_dft(self, node):
+        if node is None:
+            return
+        print(node.value)
+        self.pre_order_dft(node.left)
+        self.pre_order_dft(node.right)
+
+    # Print Post-order recursive DFT
+    def post_order_dft(self, node):
+        if node is None:
+            return
+        self.post_order_dft(node.left)
+        self.post_order_dft(node.right)
+        print(node.value)
+
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -22,7 +130,15 @@ duplicates = []  # Return the list of duplicates in this data structure
 #     if name in names_2:
 #         duplicates.append(name)
 
-duplicates = [name for name in names_1 if name in names_2]
+bst = BSTNode("")
+for name in names_1:
+    bst.insert(name)
+
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
+
+# duplicates = [name for name in names_1 if name in names_2]
 
 
 end_time = time.time()
@@ -33,3 +149,7 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+
+
+
