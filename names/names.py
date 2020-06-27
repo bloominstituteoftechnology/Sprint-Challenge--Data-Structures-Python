6bcc9044d694ebf5930c8eaa1d4b3b5a83c1f996
Michael Lowe <mrlowememphis@yahoo.com>
2020-06-27 12:21:10 -0500

    names duplicates fixed, time complexity explained

--- names/names.py
+++ names/names.py
@@ -12,15 +12,32 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# Initial runtime of 6.28 seconds
+# The nested loops result in a time complexity of O(n^2) which does not scale very well at all 
+
+# A set could be created from the second array, then check the first array against the set
+# If the set contains a name, one can simply append the name to duplicates
+
+names_2_set = set(names_2)
+duplicates = [name for name in names_1 if name in names_2_set]
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+
+# The result is a runtime of 0.00159 seconds
+# By the use of hashing, the time complexity of the check for set membership is O(1) on average
+# As compared to checking in a an array, like the first example was doing which is O(n)
+# Therefore, we end up with an overall time complexity of O(n)
+# This is only possible because the names are hashable, if they were not hashable
+# we would need to use a method that sorts the names, such as inserting to a binary search tree
+# This would still require that the elements be comparable, and would result
+# in a time complexity of O(n log n), that is the best time that sorting can be done in
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
