a8a6dec3bc4413888e85d9a2b0ffd2748eaac872
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-06-26 21:39:45 -0400

    Task 3 Complete

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -38,5 +38,22 @@ class LinkedList:
 
         return False
 
+    # If an empty list, return None because there is nothing to reverse
     def reverse_list(self, node, prev):
-        pass
+        if self.head is None:
+            return
+
+        # Find the last node, set it to head, and reverse it's next pointer to be previous
+        if node.next_node is None:
+            self.head = node
+            node.next_node = prev
+            return
+        
+        # Next node to evaluate starts at the node.next & move it once each time to loop through the list
+        next = node.next_node
+        
+        # Set node.next to be the prev value, essentially reversing it's arrow
+        node.next_node = prev
+        
+        # Recursive solution
+        self.reverse_list(next, node)
