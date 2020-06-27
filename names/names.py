b84dc94ff42d78bb6b5d5dc0017c16c3e7931324
Michael Lowe <mrlowememphis@yahoo.com>
2020-06-27 12:46:05 -0500

    reverse completed, BST added to names

--- names/names.py
+++ names/names.py
@@ -2,29 +2,66 @@ import time
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open('names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+
+f = open('names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+# Replace the nested for loops below with your improvements
+
+# Current runtime complexity: O(n^2)
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
-# Initial runtime of 6.28 seconds
-# The nested loops result in a time complexity of O(n^2) which does not scale very well at all 
+class NamesBST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-# A set could be created from the second array, then check the first array against the set
-# If the set contains a name, one can simply append the name to duplicates
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = NamesBST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = NamesBST(value)
+            else:
+                self.right.insert(value)
 
-names_2_set = set(names_2)
-duplicates = [name for name in names_1 if name in names_2_set]
+    def contains(self, target):
+        if target == self.value:
+            return True
+        if target >= self.value:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+        if target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+
+names_search_tree = NamesBST(names_1[0])
+# Runtime complexity: O(n)
+for name in names_1:
+    names_search_tree.insert(name)
+
+#Runtime complexity: O(log n)
+for name in names_2:
+    if names_search_tree.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
