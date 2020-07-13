890097195d3d02af2e0685f2787f6c82c6afa8b5
rfamilara <techknohauz.com>
2020-07-12 20:58:08 -0500

    Simplified the contains method in binary search tree -Achieved 64 duplicates which showed the names

--- names/binary_search_tree/names.py
+++ names/binary_search_tree/names.py
@@ -12,20 +12,13 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
+bst = BSTNode("")
 
-# Replace the nested for loops below with your improvements
-"""
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
-"""
+for name in names_1:
+    bst.insert(name)
 
-search_tree = BSTNode(names_1[0])
-for name in names_1[1:]:
-    search_tree.insert(name)
 for name in names_2:
-    if search_tree.contains(name):
+    if bst.contains(name):
         duplicates.append(name)
 
 end_time = time.time()
