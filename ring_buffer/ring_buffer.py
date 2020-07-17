4efeab585ac97e75d3f010ed6aaf4aaa9f5c3dec
shengjiyang <samswank@gmail.com>
2020-07-17 17:16:44 -0600

    final cleanup

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,29 +1,3 @@
-from queue import CircularQueue
-
-# Approach with CircularQueue class
-class RingBuffer_:
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.queue = CircularQueue(capacity)
-
-    def append(self, item):
-        self.queue.enqueue(item)
-        if self.queue.__len__() == self.capacity:
-            self.queue.dequeue()
-              
-    def get(self):
-        array = []
-        current = self.queue.storage.head
-        array.append(current.value)
-        
-        if current.get_next():
-            while current.get_next() is not self.queue.storage.head:
-                current = current.get_next()
-                array.append(current.value)
-                array = array[-1:] + array[:-1]
-        return array
-
-
 # Approach with an array
 # Debt owed to https://www.oreilly.com/library/view/python-cookbook/0596001673/ch05s19.html
 class RingBuffer:
@@ -46,35 +20,3 @@ class RingBuffer:
 
     def get(self):
         return self.array
-
-
-if __name__ == "__main__":
-    buffer = RingBuffer(5)
-
-    buffer.append('a')
-    buffer.append('b')
-    buffer.append('c')
-    buffer.append('d')
-    buffer.append('e')
-    buffer.append('f')
-
-    print(buffer.get())
-
-    print("\n")
-
-    buffer = RingBuffer_(5)
-
-    buffer.append('a')
-    buffer.append('b')
-    buffer.append('c')
-    buffer.append('d')
-    buffer.append('e')
-    buffer.append('f')
-
-    print(buffer.get())
-
-
-
-
-
-
