929ba885fb95356823a53967416880ded6e2ae00
Nobro777 <52586100+Nobro777@users.noreply.github.com>
2020-07-17 09:34:31 -0700

    finished with mvp i believe

--- names/names.py
+++ names/names.py
@@ -13,10 +13,71 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
+
+    def get_max(self):
+        current = self
+        
+        while current.right is not None:
+            current = current.right
+
+        return current.value
+
+    def for_each(self, fn):
+        fn(self.value)
+        if self.left:
+            self.left.for_each(fn)
+        if self.right:
+            self.right.for_each(fn)
+
+
+names = BSTNode("")
+
+for name1 in names_1:
+    names.insert(name1)
+for name2 in names_2:
+    if names.contains(name2):
+        duplicates.append(name2)
+# 2nd best efficiency 6 seconds faster than original^
+
+# duplicates = set(names_1) & set(names_2)
+# fastest way^
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,17 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        self.current_pos = 0
 
     def append(self, item):
-        pass
+        if len(self.data) < self.capacity:
+            self.data.append(item)
+        else:
+            self.data[self.current_pos] = item
+        
+        self.current_pos += 1
+        self.current_pos = self.current_pos % (self.capacity)
 
     def get(self):
-        pass
+        return self.data
