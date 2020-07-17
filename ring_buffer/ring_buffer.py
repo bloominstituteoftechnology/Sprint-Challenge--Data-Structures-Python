079524e0800b1e8cbd28f9eafcbb9758ea51eba2
Tate Keller <tatek1993@gmail.com>
2020-07-17 12:25:30 -0500

    ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.list = [None] * capacity
+        self.x = 0
 
     def append(self, item):
-        pass
+        #initialize list, have x rep the order in the array
+        self.list[self.x] = item
+
+        #if the place in the array = capacity, then set x to 0 to start the loop over
+        if self.x + 1 == self.capacity:
+            self.x = 0
+        #then increment it each time
+        else:
+            self.x = self.x + 1
 
     def get(self):
-        pass
+        return [item for item in (self.list) if item is not None]
