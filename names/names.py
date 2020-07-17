246d225ecfff6b933f0ff6dadb3263f0ff108768
Christine Fletcher <chrismis79@gmail.com>
2020-07-17 12:41:04 -0500

    runtime improved, but not returning duplicates

--- names/names.py
+++ names/names.py
@@ -13,15 +13,61 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+    #Insert value into tree
+    def insert(self, value):
+        if value < self.value:
+            #If no left child, insert value
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+        
+    
+    def containsDups(self, target):
+        if self.value == target:
+            return True
+        if self.value <= target:
+            if self.left is None:
+                return False
+            else:
+                return self.left.containsDups(target)
+        else:
+            if self.right is None:
+                return False
+            else:
+                return self.right.containsDups(target)
+
+BST = BSTNode("names")
+
+
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    BST.insert(name_1)
+    
+for name_2 in names_2:
+    if BST.containsDups(name_2):
+        duplicates.append(name_2)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+
+##original runtime = 6.6036 seconds
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
