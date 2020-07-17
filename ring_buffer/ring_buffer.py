4cab0527f775d20d3f562ae70eae4b17cdafc193
Shawn James <shawn.james@me.com>
2020-07-17 13:47:55 -0400

    Implement ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
+#FiFo - Circular Queue
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.queue = []
+        self.capacity = capacity
+        self.indexOfOldestItem = 0 #FiFo, where index 0 will be first item in and out
 
     def append(self, item):
-        pass
+        if len(self.queue) < self.capacity: #is there space available?
+            self.queue.append(item)
+        else: #if queueSize is at capacity:
+            self.queue[self.indexOfOldestItem] = item #append by overwriting oldest item
+            #update indexOfOldestItem
+            shouldReset = True if self.indexOfOldestItem == self.capacity else False
+            self.indexOfOldestItem = 0 if shouldReset else self.indexOfOldestItem + 1
 
     def get(self):
-        pass
+        return self.queue
