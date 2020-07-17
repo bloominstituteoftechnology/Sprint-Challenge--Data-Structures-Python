108daf4bf4d0179d88a377a613aefb6fca40a8c8
Griffinw15 <wilson.griffin15@gmail.com>
2020-07-17 16:16:48 -0700

    commit

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,21 +1,17 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.data = []
         self.size = 0
-        
-        #self.head = 0
-        #self.tail = -1
-        #self.data = [0 for i in range(capacity)]
+        self.data = [None for i in range(capacity)]
 
     def append(self, item):
         if self.capacity == self.size:
-            self.data.pop(0)
-            self.data.insert(0, item) 
+            self.data[0] = item
+            self.size = 1
         else:
-            self.data.append(item)
+            self.data[self.size] = item
             self.size += 1
 
     def get(self):
         if self.size > 0:
-            return self.data
+            return [i for i in self.data if i is not None]
