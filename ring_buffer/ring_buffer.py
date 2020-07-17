e38475b23bb797c727f0c8bf74c0cdb3a62fc739
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-07-17 08:26:49 -0700

    ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.queue = []
+        self.current = 0
 
     def append(self, item):
-        pass
+        # if the queue is not full, append the item. current position does not need to be updated
+        if len(self.queue) < self.capacity:
+            self.queue.append(item)
+        else:  # if the queue is full, append the new item to the item at current index, which is the oldest item in the queue
+            self.queue[self.current] = item
+
+        self.current += 1
+        # current position will not exceed capacity
+        self.current = self.current % self.capacity
 
     def get(self):
-        pass
+        return self.queue
