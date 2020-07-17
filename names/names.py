7d2c75e171312759dcddf1d1ee35a00c9cab3e1e
Patrick Millet <lohs79seahawk@gmail.com>
2020-07-17 11:00:37 -0500

    BST runtime 0.11 seconds, working on a few stretch goals.

--- names/names.py
+++ names/names.py
@@ -14,10 +14,24 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+# Using Nested for loop
+# Runtime approx 5.6 seconds
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# Using BST
+# Runtime 6.2 seconds
+
+bst = BSTNode(names_1[0])
+for name in names_1[1:]:
+    bst.insert(name)
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -27,3 +41,15 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# Using set - create set from 2nd array and compare the first array, if the set contains a matching
+# a name we append to duplicates 
+# Runtime 0.0039 seconds
+# By using a hashable value we can change the complexity to O(1)
+
+# names_2_set = set(names_2)
+
+# duplicates = [name for name in names_1 if name in names_2_set]
+
+# ----------------------------------------------------------------
+
