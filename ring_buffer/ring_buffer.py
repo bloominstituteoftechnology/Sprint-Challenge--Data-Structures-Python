54ad4025f93ac41ff1f61f591067b8a86ae0e2ed
wesley.moody <wgm3190@gmail.com>
2020-07-17 11:49:37 -0500

    refactored ring_buffer, no tests were passing but all 6 pass now

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,16 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        self.capacity = capacity
         self.data = []
+        self.capacity = capacity   
         self.oldest_index = 0
 
     def append(self, item):
         # append an item at the end of the buffer
-        self.data.append(item)
-        if len(self.data) == capacity:
-            self.current = 0
-            # permanently chang self class from not full to full
-            self.__class__ = self.__Full
+        if len(self.data) < self.capacity:
+            self.data.append(item)
+        else:
+            self.data[self.oldest_index] = item
+            self.oldest_index +=1
+        if self.oldest_index == self.capacity:
+            self.oldest_index = 0
+            
 
     def get(self):
         return self.data
