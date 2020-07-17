de5a86a0295fb9e0703484e1cfebe1259a84fe31
shengjiyang <samswank@gmail.com>
2020-07-17 09:14:13 -0600

    completed names excercise

--- names/names.py
+++ names/names.py
@@ -1,22 +1,54 @@
 import time
+from itertools import filterfalse
 
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
+f = open('/Users/samuel/Programming/GitHub/Lambda School/Assignments/Sprint-Challenge--Data-Structures-Python/names/names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names_2.txt', 'r')
+f = open('/Users/samuel/Programming/GitHub/Lambda School/Assignments/Sprint-Challenge--Data-Structures-Python/names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# runtime: 106.52314519882202 seconds
+# My machine is really slow;
+# good thing I'll be building a better one soon.
+
+print(type(names_1), type(names_2)) # --> Both are lists/arrays
+names = names_1 + names_2
+
+# From https://docs.python.org/3.7/library/itertools.html#itertools-recipes
+def unique_everseen(iterable, key=None):
+    "List unique elements, preserving order. Remember all elements ever seen."
+    # unique_everseen('AAAABBBCCDAABBB') --> A B C D
+    # unique_everseen('ABBCcAD', str.lower) --> A B C D
+    seen = set()
+    seen_add = seen.add
+    if key is None:
+        for element in filterfalse(seen.__contains__, iterable):
+            seen_add(element)
+            yield element
+    else:
+        for element in iterable:
+            k = key(element)
+            if k not in seen:
+                seen_add(k)
+                yield element
+
+generator = unique_everseen(names)
+for name in generator:
+    duplicates.append(name)
+
+# runtime: 0.05473685264587402 seconds, much faster:)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
