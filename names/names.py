4d29da5ec881682f4ded5c7783064176ec2af539
john <jgreenberg@runbox.com>
2020-07-17 15:01:32 -0400

    added

--- names/names.py
+++ names/names.py
@@ -13,10 +13,12 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+#for name_1 in names_1: O (n)
+    #for name_2 in names_2: O (n)
+        #if name_1 == name_2:
+            #duplicates.append(name_1)
+for dupe in set(map(str.rstrip,names_1)).intersection(map(str.rstrip,names_2)):
+            duplicates.append(dupe)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
