8f90b6ecd2df2545bb03a868335e41e87161aea5
Vasuki Sunder <vasukisunder@Vasukis-MBP.attlocal.net>
2020-07-17 08:41:33 -0700

    completed ringbuffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        self.position = 0  #index of current position
+
 
     def append(self, item):
-        pass
+        if len(self.data) < self.capacity:
+            self.data.append(item)
+        else:
+            self.data[self.position] = item
+
+        self.position += 1
+
+        if self.position == self.capacity:
+            self.position = 0
+
 
     def get(self):
-        pass
+        return self.data
