6ff738ebae5a249ce4221ad4b1ce7372cfd5e0bc
modares <modareshadi@gmail.com>
2020-07-17 09:27:33 -0700

    done with MVP

---
+++ names/names_optimized.py
@@ -0,0 +1,32 @@
+import time
+from binary_search_tree import BSTNode
+
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates = []  # Return the list of duplicates in this data structure
+
+# Replace the nested for loops below with your improvements
+names_1_BST = BSTNode(names_1[0])
+for name in names_1:
+    names_1_BST.insert(name)
+
+for name in names_2:
+    if names_1_BST.contains(name):
+        duplicates.append(name)
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
+# ---------- Stretch Goal -----------
+# Python has built-in tools that allow for a very efficient approach to this problem
+# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# structures, but you may not import any additional libraries that you did not write yourself.
