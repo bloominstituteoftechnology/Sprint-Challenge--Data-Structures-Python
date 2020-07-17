85a7fe53970bd5949311524b7764499b539ec9fb
Teague <teaguehannam@gmail.com>
2020-07-17 09:31:04 -0600

    names complete

--- names/names.py
+++ names/names.py
@@ -1,22 +1,37 @@
 import time
-
 start_time = time.time()
 
-f = open('names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
+with open('names_1.txt', 'r') as file:
+	names_1 = file.read().split("\n") 
+file.close()
+with open('names_2.txt', 'r') as file:
+	names_2 = file.read().split("\n") 
+file.close()
 
-f = open('names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-f.close()
+duplicates = []
 
-duplicates = []  # Return the list of duplicates in this data structure
+# Stretch 0(1) (0.008 seconds) 
+# Dictionaries are hashed, do no need to iterate over everything
+dictionary1 = {names_1[index] : index for index in range(0,10000)}
+dictionary2 = {names_2[index] : index for index in range(0,10000)}
 
+for name_1 in dictionary1:
+	if name_1 in dictionary2:
+		duplicates.append(name_1)
+
+''' Version 2 O(n) (1.35 seconds)
+for name_1 in names_1:
+	if name_1 in names_2:
+		duplicates.append(name_1)
+'''
+
+''' Version 1 O(n^x) (5.64 seconds)
 # Replace the nested for loops below with your improvements
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
+'''
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -24,5 +39,5 @@ print (f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# What's the best time you can accomplish?  There are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
