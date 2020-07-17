8592ca92cb54ffb051a1fef0d0e23344381bda94
Enrique Gongora <EGTech24@gmail.com>
2020-07-17 08:30:57 -0700

    Runtime optimization completed

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from bst import BSTNode
 
 start_time = time.time()
 
@@ -13,10 +14,19 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+bst_node = BSTNode("")
+
+# We'll need to loop through all the names in 'names_1' file
+for name in names_1:
+    # use the 'insert' method to add each name
+    bst_node.insert(name)
+
+# Loop through all the names in 'name_2' file
+for name in names_2:
+    # Use an if statement to check if the tree has identical names 
+    if bst_node.contains(name):
+        # in the case it does, append those names to the duplicated list
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
