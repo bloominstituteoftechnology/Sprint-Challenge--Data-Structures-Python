79c2992125679020f184cccc4da78abd25bea63d
Enrique Gongora <EGTech24@gmail.com>
2020-07-17 10:01:32 -0700

    Reverse & ring buffer completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,29 @@
 class RingBuffer:
+    # this implements a buffer that isn't full yet
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        # the oldest item is at the 0 index
+        self.oldest_item = 0
+
 
     def append(self, item):
-        pass
+        # if the storage is less than the total capacity
+        if len(self.storage) < self.capacity:
+            # append the item
+            self.storage.append(item)
+            # if there storage isn't less than the total capacity
+        else:
+            # "append" an item by overwriting the oldest item
+            self.storage[self.oldest_item] = item
+            # verify which item is the oldest
+            if self.oldest_item < len(self.storage) - 1:
+                # increment by 1 so we can go through the list
+                self.oldest_item += 1
+            else:
+                # remove the first item or "oldest" item in the list
+                self.oldest_item = 0
 
     def get(self):
-        pass
+        # returns a list of elements from the oldest to the newest
+        return self.storage
