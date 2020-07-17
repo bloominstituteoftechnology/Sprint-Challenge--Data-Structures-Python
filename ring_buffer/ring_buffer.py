c029479f7e7df85153090238738fbddba9672f3d
wesley.moody <wgm3190@gmail.com>
2020-07-17 10:32:06 -0500

    ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,10 +1,16 @@
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        
+        self.data = []
+        self.oldest_index = 0
 
     def append(self, item):
-        pass
+        # append an item at the end of the buffer
+        self.data.append(item)
+        if len(self.data) == capacity:
+            self.current = 0
+            # permanently chang self class from not full to full
+            self.__class__ = self.__Full
 
     def get(self):
-        pass
+        return self.data
