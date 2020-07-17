96266f922194a10ff95899e652fd4d62fecca747
Krishna Dahal <krishna.dahal2017@gmail.com>
2020-07-17 13:08:30 -0400

    changes done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,17 +1,19 @@
 class RingBuffer:
-    def __init__(self, capacity, store=[]):
-        self.index = 0
+    def __init__(self, capacity):
+        self.last_item = 0 # oldest item is at index 0
         self.capacity = capacity
-        self.store = list(store)[capacity:]
+        self.store = []
 
 
     def append(self, item):
-        if len(self.store) == self.capacity:
-            self.store[self.index] = item
-        else:
-            self.store.append(item)
-        self.index = (self.index + 1) % self.capacity
-
+        if len(self.store) < self.capacity:
+            self.store.append(item) # stores new item when there is space available in capacity
+        else:   # capacity and store either full or store has more items than capacity
+            self.store[self.last_item] = item
+            if self.last_item < (len(self.store) - 1):
+                self.last_item += 1
+            else:
+                self.last_item = 0
 
     def get(self):
         return(self.store)
