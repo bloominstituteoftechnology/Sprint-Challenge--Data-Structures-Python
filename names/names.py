389a08d2ec12e53bb3780e7fe93107a90c39055b
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-07-17 12:28:47 -0400

    Completes names.py with runtime below 1 second

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from bst import BinarySearchTree
 
 start_time = time.time()
 
@@ -13,16 +14,49 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+"""
+You may not use the built in Python list, set, or dictionary in your solution for
+this problem. However, you can and should use the provided duplicates list to
+return your solution.
+"""
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# ABW Solution II: using binary search tree (solution I was a fail!)
+bst = BinarySearchTree("A")
+
+for x in names_1:
+    bst.insert(x)
+
+for y in names_2:
+    if bst.contains(y):
+        duplicates.append(bst.contains(y))
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
+
+# Time on First Run: 5.129 seconds
+# Time on ABW Solution II: 0.111 seconds
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
-# structures, but you may not import any additional libraries that you did not write yourself.
+# structures, but you may not import any additional libraries that you did
+# not write yourself.
+
+
+# if __name__ == "__main__":
+#     my_list = [1,2,3,4]
+#     other_list = [5,2,7,4]
+#     dupes = []
+
+#     for i,x in product(my_list, other_list):
+#         if i == x:
+#             dupes.append(i)
+
+#     print(dupes)
+#     # breakpoint()
