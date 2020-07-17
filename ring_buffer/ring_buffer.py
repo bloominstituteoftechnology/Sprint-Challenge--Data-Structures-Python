63568b879fbdbd0a1d0d0415c2730ac55021afe4
Reagan Barrington <reagan@barringtonfamily.net>
2020-07-17 11:40:38 -0400

    ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,23 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.queue = [None] * capacity
+        self.tail = -1
+        self.head = 0
+        self.size = 0
 
     def append(self, item):
-        pass
+        # Use of %: Now every time we get to the end of the array we will automatically start back at zero, allowing us to circularly queue.
+        self.tail = (self.tail + 1) % self.capacity
+        self.queue[self.tail] = item
+        self.size = self.size + 1
+        
 
     def get(self):
-        pass
+        # ignore all that are None
+        result = []
+        for item in self.queue:
+            if item is not None:
+                result.append(item)
+        return result
+            
