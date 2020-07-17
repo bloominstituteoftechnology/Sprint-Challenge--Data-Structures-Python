576d449a836283694765f2e65e875fd6eaa59d80
modares <modareshadi@gmail.com>
2020-07-17 10:09:58 -0700

    counldn't figure out 2nd stretch goal

---
+++ names/names_optimized_readme_stretch.py
@@ -0,0 +1,38 @@
+import time
+from binary_search_tree import BSTNode
+
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates = []  # Return the list of duplicates in this data structure
+
+# Replace the nested for loops below with your improvements
+names_1.sort()
+names_2.sort()
+flag = 0
+for name_1 in names_1:
+    flag = 0
+    for name_2 in names_2:
+        if name_1[0] > name_2[0]:
+            flag = 1
+            continue
+        if name_1 == name_2:
+            duplicates.append(name_1)
+    if flag == 1:
+        continue
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
+# ---------- Stretch Goal -----------
+# Python has built-in tools that allow for a very efficient approach to this problem
+# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# structures, but you may not import any additional libraries that you did not write yourself.
