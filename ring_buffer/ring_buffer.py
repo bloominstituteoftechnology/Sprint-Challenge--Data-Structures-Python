e91013d12e7895d7e209872e14fde5dc3ad2b095
padgettriver <padgettcriver@gmail.com>
2020-07-17 13:47:27 -0400

    MVP

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.data = []
+        self.capacity = capacity
+        self.run = 0
 
     def append(self, item):
-        pass
+        if len(self.data) < self.capacity:
+            self.data.append(item)
+        else:
+            self.data.pop(self.run)
+            self.data.insert(self.run, item)
+            if self.run < self.capacity - 1:
+                self.run += 1
+            else:
+                self.run = 0
 
     def get(self):
-        pass
+        return self.data
