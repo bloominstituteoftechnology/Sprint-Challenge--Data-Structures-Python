101ec22c2a30fb5e389307119f736ccdd6a68458
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-07-17 09:31:01 -0700

    Implemented ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,26 @@
+from collections import deque
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        # this is a constructor list which will take a parameter capacity and iterate til the end
+        self.items = list()[-capacity:]
+        self.ring_position = 0
+
+
 
     def append(self, item):
-        pass
+       # if the len of the items is equal to the capacity then we will append items with its position
+       # to the item
+       if len(self.items) == self.capacity:
+           self.items[self.ring_position] = item
+           # if the items are lesser then capacity then we will append the item to items as new item.
+       if len(self.items) < self.capacity:
+           self.items.append(item)
+
+
+
+
 
     def get(self):
-        pass
+        return self.items
