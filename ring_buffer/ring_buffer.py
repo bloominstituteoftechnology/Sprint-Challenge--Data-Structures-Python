dce57cacfec9da722b385c62366b733274b07545
Wesley Mountford <goe_horus@hotmail.com>
2020-07-17 10:56:51 -0500

    Did sprint challenge

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.full = False
 
     def append(self, item):
-        pass
+        if self.full:
+            self.storage[self.oldest_index] = item
+            self.oldest_index = (self.oldest_index+1) % self.capacity
+        else:
+            self.storage.append(item)
+            if len(self.storage) == self.capacity:
+                self.full = True
+                self.oldest_index = 0
 
     def get(self):
-        pass
+        return self.storage
+
