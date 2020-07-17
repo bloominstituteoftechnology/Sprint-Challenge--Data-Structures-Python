a150b122e2d500b1c9fd729dd868994f93557379
john <jgreenberg@runbox.com>
2020-07-17 12:27:34 -0400

    module 1

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
 
     def append(self, item):
-        pass
+        self.data.append(item) 
+        if len(self.data) == self.capacity: 
+            self.counter = 0
+            self.__class__ = self.CapacityMax
 
     def get(self):
-        pass
+        return self.data
+
+    class CapacityMax: 
+        def append(self, item): 
+            self.data[self.counter] = item
+            self.counter = (self.counter + 1) % self.capacity
+
+        def get(self): 
+            return self.data
