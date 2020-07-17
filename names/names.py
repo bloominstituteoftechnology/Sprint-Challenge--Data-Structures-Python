f083eef48386d2396755cecd4c77766cc1b29aaf
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-07-17 08:11:33 -0700

    reduced runtime in names.py

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from bst import BSTNode
 
 start_time = time.time()
 
@@ -13,14 +14,25 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+search_tree = BSTNode(names_2[0])
+
+for name in names_2[1:]:
+    search_tree.insert(name)
+
+# search_tree.in_order_print()
+
+for name in names_1:
+    if search_tree.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
