4795032f1faeebdfea60b1c7e61673f406cb3ff9
Alex Shillingford <alex.shillingford@icloud.com>
2020-08-01 12:02:32 -0600

    Reduce runtime from 4 seconds to 1 second

--- names/names.py
+++ names/names.py
@@ -10,13 +10,17 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+# duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# This method produces O(n^2) time complexity, I think O(N) Space
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# I assume this is O(N) space and time
+duplicates = [name for name in names_1 if name in names_2]
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
