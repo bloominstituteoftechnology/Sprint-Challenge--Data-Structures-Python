3a2c3fefc20459450a7ed42d241d916ab1c21a52
rubyclaguna <rubyclaguna@gmail.com>
2020-07-31 18:32:52 -0700

    ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity 
+        self.data = [] 
+        self.index = 0 
 
     def append(self, item):
-        pass
+        if len(self.data) < self.capacity: 
+            self.data.append(item)
+
+        else: 
+            self.data[self.index] = item 
+
+        self.index += 1 
+        
+        if self.index == self.capacity: 
+            self.index = 0 
 
     def get(self):
-        pass
+        return self.data
