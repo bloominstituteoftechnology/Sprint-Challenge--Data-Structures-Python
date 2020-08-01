3e316209b08d813ffdfd2222f7cc720f15527a6d
Brian Hague <alphaseinor@gmail.com>
2020-07-31 20:08:09 -0500

    solved ringbuffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,15 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.queue = []
+        self.index = 0
 
     def append(self, item):
-        pass
+        if len(self.queue) < self.capacity:
+            self.queue.append(item)
+        else:
+            self.queue[self.index] = item
+            self.index = (self.index+1) % self.capacity
 
-    def get(self):
-        pass
+    def get(self): #return the queue
+        return self.queue
