b4a1608114d60d511d27be799e7889d999fd6760
Sal B Amer <iosdevsalman@gmail.com>
2020-07-31 18:49:13 -0700

    imported DoublyLinkList to file - Added Append

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
+from doubly_linked_list import DoublyLinkedList
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.storage.length == self.capacity:
+            # remove item from head
+            remove_item = self.storage.head
+            self.storage.remove_from_head()
+            #append new item to tail
+            self.storage.add_to_tail(item)
+            if remove_item == self.current:
+                self.current = self.storage.tail
 
     def get(self):
         pass
