7ff3fe03352d262ff1048ba9d82e0e9a624657e5
Abdulhamid Azizy <55733008+HamidAzizy@users.noreply.github.com>
2020-08-03 16:41:29 -0700

    Project Done

--- names/names.py
+++ names/names.py
@@ -13,10 +13,15 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+# duplicates = set(names_1) & set(names_2)
+
+dic = {n for n in names_2}
+[duplicates.append(name) for name in names_1 if name in dic]
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
