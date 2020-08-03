6e49fddb90e01dec3fecaf2c46aa4b697bbda923
Rob LeCheminant <roblecheminant@gmail.com>
2020-08-02 19:59:02 -0600

    uploading extremely efficient code for names.py

--- names/names.py
+++ names/names.py
@@ -10,13 +10,24 @@ f = open('names/names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+# ------------ original inefficient code -------------------
+""" The timing of this ^^^ code took approximately 7.87 seconds. """
+# duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# # Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+# -----------------------------------------------------------
+
+
+
+# More efficient line
+""" This code took approximately 0.005 seconds! """
+duplicates = set(names_1) & set(names_2)
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -26,3 +37,5 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+""" The fastest I could find is implemented above... 0.005 seconds! """ 
