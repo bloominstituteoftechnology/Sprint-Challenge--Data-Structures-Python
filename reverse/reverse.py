9b7a509c59fd0cf0d0040c3dd5f3f3d03aa21a91
john <john@spinningplates.org>
2020-08-02 20:10:23 -0700

    reverse.py - test passed

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,10 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+    def __str__(self):
+        return f"Value: {self.value}, Next_Node: {self.next_node}"
+
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -23,6 +27,9 @@ class LinkedList:
             node.set_next(self.head)
 
         self.head = node
+
+        print("ADD TO HEAD")
+        print(f"Head: {self.head}")
 
     def contains(self, value):
         if not self.head:
@@ -39,4 +46,43 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+
+        # check if LL is empty
+        if self.head is None:
+            return
+
+        # store current node
+        cur_node = node
+        # store next node
+        next_node = cur_node.next_node
+        # make next node the prev
+        cur_node.set_next(prev)
+
+        # loop through all nodes until None
+        while next_node is not None:
+
+            # store previous node
+            prev_node = cur_node
+
+            # store current node
+            cur_node = next_node
+
+            # store next node of current node
+            next_node = cur_node.get_next()
+
+            # reverse cur_node pointer to previous node
+            cur_node.set_next(prev_node)
+
+        # set head to new current node
+        self.head = cur_node
+
+        print("REVERSE LIST")
+        print(f"Head: {self.head}")
+
+
+linked_list = LinkedList()
+
+linked_list.add_to_head(3)
+linked_list.add_to_head(2)
+linked_list.add_to_head(1)
+linked_list.reverse_list(linked_list.head, None)
