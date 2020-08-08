49f84bca4826205e780f47871931d8540dcaef19
OliverAbreu <oliverabreu@localhost.localdomain>
2020-08-07 23:12:15 -0400

    This commit should record the solution to

--- names/names.py
+++ names/names.py
@@ -1,5 +1,31 @@
 import time
 
+class BinarySearchTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left:
+                self.left.insert(value)
+            else:
+                self.left = BinarySearchTree(value)
+        else:
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BinarySearchTree(value)
+
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif target < self.value and self.left:
+            return self.left.contains(target)
+        elif target > self.value and self.right:
+            return self.right.contains(target)
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -12,11 +38,14 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+
+binarySearchTree = BinarySearchTree("None")
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+for name in names_1:
+    binarySearchTree.insert(name)
+for name in names_2:
+    if binarySearchTree.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
