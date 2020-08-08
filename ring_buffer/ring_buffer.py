fa11aa89ff3d1a631d37ac52cf8dfb5aacfaa350
OliverAbreu <oliverabreu@localhost.localdomain>
2020-08-07 22:43:09 -0400

    This commit should initialize the repository and record the solution to

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,17 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+        self.queue_position = 0
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        else:
+            self.storage[self.queue_position] = item
+        self.queue_position += 1
+        if self.queue_position == self.capacity:
+            self.queue_position = 0
 
     def get(self):
-        pass
+        return self.storage
