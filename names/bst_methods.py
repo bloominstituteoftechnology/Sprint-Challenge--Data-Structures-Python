90cb052739f677b877aa928a27b1674e7a522681
Veto Ramirez <vetorobert3@gmail.com>
2020-08-09 19:31:36 -0700

    working on the name challenge for the sprint.

---
+++ names/bst_methods.py
@@ -0,0 +1,26 @@
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if self.value >= target:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contain(target)
