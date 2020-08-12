d08d365d67e2ad6038867d3847cddcdbc48938c9
Pyrom <56746576+e-pyrom@users.noreply.github.com>
2020-08-11 20:39:36 -0400

    names.py binary search tree

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from BST import BSTNode
 
 start_time = time.time()
 
@@ -13,10 +14,19 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+#for name_1 in names_1:
+#    for name_2 in names_2:
+#        if name_1 == name_2:
+#            duplicates.append(name_1)
+
+bst = BSTNode("None")
+
+for name in names_1:  
+    bst.insert(name)
+
+for name in names_2: 
+    if bst.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -26,29 +36,3 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
-
-start_time = time.time()
-
-f = open('/Users/pyrome/Desktop/Lambda/Sprint-Challenge--Data-Structures-Python/names/names_1.txt', 'r')
-names_1 = f.read().split("\n")  # List containing 10000 names
-f.close()
-
-f = open('/Users/pyrome/Desktop/Lambda/Sprint-Challenge--Data-Structures-Python/names/names_2.txt', 'r')
-names_2 = f.read().split("\n")  # List containing 10000 names
-f.close()
-
-seen = {}
-duplicates = []
-
-for name in list(names_2 + names_1):
-    if name not in seen:
-        seen[name] = 1
-    else:
-        if seen[name] == 1:
-            duplicates.append(name)
-        seen[name] += 1
-
-end_time = time.time()
-
-print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print(f"runtime: {end_time - start_time} seconds")
