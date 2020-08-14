7d41340581a724504334a2f6b243741b9e45bb5d
Harmony Radley <harmonyradley99@gmail.com>
2020-08-14 12:40:00 -0400

    mvp

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity # initialize variable
+        self.storage = [] # Empty list to hold the data
+        self.index = 0 # This is an index variable with initial value of zero.
+
 
     def append(self, item):
-        pass
+        # if  the length of storage is less than capacity append item to list
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        else: # This is for if we are at or over capacity
+            self.storage[self.index] = item # This is overwriting the existing item beginning at index 0 
+            self.index += 1 # move the index to the next item in list
+            if self.index == self.capacity: # This is checking if the index has reached max capacity
+                self.index = 0 # if the answer is yes, we want to reset the index to 0
 
     def get(self):
-        pass
+        # return the list.
+        return self.storage
