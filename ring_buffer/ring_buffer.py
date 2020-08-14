a5a69764f9756dae3102e2cbecce6d485617ec05
nimu77 <nirmal.thapa77@outlook.com>
2020-08-14 10:57:53 -0700

    nirmal_sprint_challenge_week2_cs34

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,16 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
 
     def append(self, item):
-        pass
+        
+
+        if len(self.data) >= self.capacity:
+            self.data.pop(0) and self.data.insert(0, item)
+            
+        else:
+            self.data.append(item)
 
     def get(self):
-        pass
+        return self.data
