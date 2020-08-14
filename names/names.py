4d88dd3ebad35f54bcff4b5f6c6c601625aaca8b
Chris <chris.gonzales.82@gmail.com>
2020-08-14 08:49:52 -0700

    Duplicate names optimized

--- names/names.py
+++ names/names.py
@@ -1,5 +1,45 @@
 import time
 
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add_to_head(self, value):
+        node = Node(value)
+
+        if self.head is not None:
+            node.set_next(self.head)
+
+        self.head = node
+
+    def contains(self, value):
+        if not self.head:
+            return False
+
+        current = self.head
+
+        while current:
+            if current.get_value() == value:
+                return True
+
+            current = current.get_next()
+
+        return False
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -12,11 +52,24 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+
+list_names1 = LinkedList()
+
+for name in names_1:
+    list_names1.add_to_head(name)
+
+list_names2 = LinkedList()
+
+for name in names_2:
+    list_names2.add_to_head(name)
+    if list_names1.contains(name):
+        duplicates.append(name)
+
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -26,3 +79,5 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# duplicates = [set(names_1) & set(names_2)]
