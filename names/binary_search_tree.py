7d123b3548d0713b17c8e148e3a79a6e97f0b8d0
Griffinw15 <wilson.griffin15@gmail.com>
2020-08-14 09:24:55 -0700

    mvp i think3

--- names/binary_search_tree.py
+++ names/binary_search_tree.py
@@ -153,31 +153,3 @@ class BSTNode:
                 queue.append(current.right)
 
             fn(current.value)
-
-    # Part 2 -------------
-
-    # Print all the values in order from low to high
-    # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self, node):
-        pass
-
-    # Print the value of every node, starting with the given node,
-    # in an iterative breadth first traversal
-    def bft_print(self, node):
-        pass
-
-    # Print the value of every node, starting with the given node,
-    # in an iterative depth first traversal
-    def dft_print(self, node):
-        pass
-
-    # Stretch Goals -------------------------
-    # Note: Research may be required
-
-    # Print Pre-order recursive DFT
-    def pre_order_dft(self, node):
-        pass
-
-    # Print Post-order recursive DFT
-    def post_order_dft(self, node):
-        pass
