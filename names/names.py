277926cd258299bbdc7d04066bd09c93ddb48245
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-08-14 09:15:22 -0700

    MVP

--- names/names.py
+++ names/names.py
@@ -1,5 +1,7 @@
 import time
 
+from binary_search_tree import BinarySearchTree
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -12,17 +14,25 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+binary_tree = BinarySearchTree('names')
+
+for names in names_1:
+    binary_tree.insert(names)
+
+for name in names_2:
+    if binary_tree.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
+
+# INITIAL RUNTIME: 6.331447124481201 seconds
+
+# OPTIMIZED RUNTIME: 0.10910296440124512 seconds
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# What's the best time you can accomplish?  There are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,4 +1,4 @@
-from ring_buffer.Doubly_linked_list import DoublyLinkedList
+from Doubly_linked_list import DoublyLinkedList
 
 
 class RingBuffer:
@@ -9,26 +9,45 @@ class RingBuffer:
 
     def append(self, item):
         # If storage is 0 change current to None
+        if self.current is None:
+            self.current = 0
         # If storage is full
-        # Get Head
-        # Go to the index we're replacing
-        # Replace Value
-        # If we've reached overflow
-        # Set back to None
-        # increment upwards
-        #return
-        # add to tail
+        if self.storage.length == self.capacity:
+            # Get Head
+            node = self.storage.head
 
+            if self.current > 0:
+                # Go to the index we're replacing
+                for _ in range(0, self.current):
+                    node = node.next
+            # Replace Value
+            node.value = item
+            # If we've reached overflow
+            if self.current == self.capacity - 1:
+                # Set back to None
+                self.current = None
+            else:
+                # increment upwards
+                self.current += 1
+            # return
+            return
+        # add to tail
+        self.storage.add_to_tail(item)
         pass
 
     def get(self):
-
+        list_buffer_contents = []
         # get head
 
         # while head is none
+        head = self.storage.head
 
-        # add to list
-        # go  to next
+        while head:
+            # add to list
+            list_buffer_contents.append(head.value)
+            # go  to next
+            head = head.next
 
         # return
+        return list_buffer_contents
         pass
