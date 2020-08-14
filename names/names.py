64cd7fea606b0b969c0598ab979b8d6851a59c7e
karina-rodriguez <karminer60@gmail.com>
2020-08-14 10:01:26 -0700

    done with names.py

--- names/names.py
+++ names/names.py
@@ -10,14 +10,10 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-duplicates = []  # Return the list of duplicates in this data structure
+duplicates= []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
 
-#for name_1 in names_1:
-    #for name_2 in names_2:
-        #if name_1 == name_2:
-            #duplicates.append(name_1)
 
 class BSTNode:
     def __init__(self, value):
@@ -25,9 +21,34 @@ class BSTNode:
         self.left = None
         self.right = None
 
-    def duplicates(self):
-        if name_1 == name_2:
-            duplicates.append(name)
+    def insert(self, value):
+        #find value
+        if self.value == value:
+            duplicates.append(value)
+            return
+        if value < self.value:
+            if self.left is not None:
+                self.left.insert(value)
+            else: 
+                self.left = BSTNode(value)
+                
+ 
+        else:
+            if self.right is not None:
+                self.right.insert(value)
+            else: 
+                self.right = BSTNode(value)
+
+tree = BSTNode(names_1[0])
+for name_1 in names_1[1:]:
+    tree.insert(name_1)
+    
+for name_2 in names_2:
+    tree.insert(name_2)
+
+
+                    
+            
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
