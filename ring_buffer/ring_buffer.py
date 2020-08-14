67835845661ca019b476313b4bb1f07c7a37ce1a
karina-rodriguez <karminer60@gmail.com>
2020-08-14 13:48:28 -0700

    done with ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -3,12 +3,19 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.data = []
+        self.location = 0
 
     def append(self, item):
+        
+        if self.capacity == len(self.data): 
+            if self.location < self.capacity - 1:
+                self.data[self.location] = item
+                self.location += 1
+            else:
+                self.data[self.location] = item
+                self.location = 0
+         
 
-        if self.capacity == len(self.data):
-            
-     
         else:
             self.data.append(item)        
   
