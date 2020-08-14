c9da4addaa9beeed8ee6a61db235a8174486c5f1
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-08-14 08:34:19 -0700

    Initial Commit

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,34 @@
+from ring_buffer.Doubly_linked_list import DoublyLinkedList
+
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.current = None
+        self.storage = DoublyLinkedList()
 
     def append(self, item):
+        # If storage is 0 change current to None
+        # If storage is full
+        # Get Head
+        # Go to the index we're replacing
+        # Replace Value
+        # If we've reached overflow
+        # Set back to None
+        # increment upwards
+        #return
+        # add to tail
+
         pass
 
     def get(self):
+
+        # get head
+
+        # while head is none
+
+        # add to list
+        # go  to next
+
+        # return
         pass
