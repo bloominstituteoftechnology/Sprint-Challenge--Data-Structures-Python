a6bad3bcc710a4e9bb433f449de13fa0039337d7
GraceeElton <60789216+GraceeElton@users.noreply.github.com>
2020-08-19 15:47:21 -0600

    names done

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BSTNode
 
 start_time = time.time()
 
@@ -13,28 +14,25 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # you got this! think simple.
+# first you need to add a root
+root = BSTNode(names_1[0])
+for first in names_1:
+    root.insert(first)
+for second in names_2:
+    if root.contains(second):
+        duplicates.append(second)
 
-
-def find_dupes(list1, list2):
-    for item in list1:
-        if item in list2:
-            duplicates.append(item)
-
-
-find_dupes(names_1, names_2)
-
-# Replace the nested for loops below with your improvements
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
+    # Replace the nested for loops below with your improvements
+    # for name_1 in names_1:
+    #     for name_2 in names_2:
+    #         if name_1 == name_2:
+    #             duplicates.append(name_1)
 
 end_time = time.time()
 print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print(f"runtime: {end_time - start_time} seconds")
 
 # orginal  runtime: 7.794878959655762 seconds
-# my time runtime: 1.7154271602630615 seconds proud
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
