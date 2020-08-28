15358b1ccd092ed1a809efe580e1c8a866c5ef75
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-08-28 19:21:10 -0400

    Initial Commit with Ryan_D_Sizemore branch

--- names/names.py
+++ names/names.py
@@ -2,76 +2,27 @@ import time
 
 start_time = time.time()
 
-f = open('names/names_1.txt', 'r')
+f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-f = open('names/names_2.txt', 'r')
+f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-# Current runtime complexity: O(n^2)
-# for name_1 in names_1:
-#     for name_2 in names_2:
-#         if name_1 == name_2:
-#             duplicates.append(name_1)
-
-class NamesBST:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-    
-    def insert(self, value):
-        if value < self.value:
-            if self.left is None:
-                self.left = NamesBST(value)
-            else:
-                self.left.insert(value)
-        else:
-            if self.right is None:
-                self.right = NamesBST(value)
-            else:
-                self.right.insert(value)
-
-    def contains(self, target):
-        if target == self.value:
-            return True
-        if target >= self.value:
-            if self.right is None:
-                return False
-            else:
-                return self.right.contains(target)
-        if target < self.value:
-            if self.left is None:
-                return False
-            else:
-                return self.left.contains(target)
-
-names_search_tree = NamesBST(names_1[0])
-# Runtime complexity: O(n)
-for name in names_1:
-    names_search_tree.insert(name)
-
-#Runtime complexity: O(log n)
-for name in names_2:
-    if names_search_tree.contains(name):
-        duplicates.append(name)
+for name_1 in names_1:
+    for name_2 in names_2:
+        if name_1 == name_2:
+            duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-# Runtime: ~ 0.058 seconds
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
-
-# stretch_duplicates = [n for n in names_1 if n in names_2]
-# print (f"{len(stretch_duplicates)} duplicates:\n\n{', '.join(stretch_duplicates)}\n\n")
-# print (f"runtime: {end_time - start_time} seconds")
-# Runtime: ~ 0.057 seconds
--- reverse/reverse.py
+++ reverse/reverse.py
@@ -37,24 +37,6 @@ class LinkedList:
             current = current.get_next()
 
         return False
-    
-    # If an empty list, return None because there is nothing to reverse
+
     def reverse_list(self, node, prev):
-        
-        if self.head is None:
-            return
-
-        # If it's the last node, set it to head, and reverse it's next pointer to be previous
-        if node.next_node is None:
-            self.head = node
-            node.next_node = prev
-            return
-        
-        # Else, we have pointers: 
-        # next to evaluate the start node's next value to be able to hit each element in the list
-        # node.next_node set to the prev value of the current node, reversing it's arrow (initialized as None if called on head node)
-        next = node.next_node
-        node.next_node = prev
-
-        # Recursive solution to move through the nodes in the list
-        self.reverse_list(next, node)
+        pass
