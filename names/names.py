08ab7cf7058a21bc3ff6631c55b14ea74e8d405c
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-08-28 19:53:27 -0400

    ring buffer with names no testing

--- names/names.py
+++ names/names.py
@@ -13,10 +13,32 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+
+# Current runtime complexity: O(n^2)
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
+            
+class NamesBST:
+    def __init__(self,value):
+        self.value = value
+        self.left = None
+        self.right = None
+        
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = NamesBST(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = NamesBST(value)
+            else:
+                self.right.insert(value)
+                
+                
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
