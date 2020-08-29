f147a16cee71f20bb9c445fa8f317255e3056c47
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-08-28 20:33:37 -0400

    MVP

--- names/names.py
+++ names/names.py
@@ -65,7 +65,10 @@ end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+# Runtime: approx 0.051 seconds
+
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
