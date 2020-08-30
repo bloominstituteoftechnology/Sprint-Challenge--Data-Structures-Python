664bdfe2d1396d287883ba2e1d2e47c3123b8a1e
Lambda_School_Loaner_172 <jpldeuce@gmail.com>
2020-08-30 19:28:55 -0400

    ring_buffer.py completed and passing

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        self.counter = 0
 
     def append(self, item):
-        pass
+        if len(self.data) == self.capacity:
+            self.data[self.counter] = item
+            if self.counter + 1 == self.capacity:
+                self.counter = 0
+            else:
+                self.counter += 1
+        else:
+            self.data.append(item)
+        return item
 
     def get(self):
-        pass
+        return self.data
