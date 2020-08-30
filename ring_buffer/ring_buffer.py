8736b05d99ae8c6ecc29754a45e03a2e83be88c1
Matthew Taylor <majikpig@gmail.com>
2020-08-30 19:21:44 -0400

    ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,19 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.storage = []
+        self.capacity = capacity
+        self.pointer = 0
+        self.length = 0
 
     def append(self, item):
-        pass
+        if self.length < self.capacity:
+            self.storage.append(item)
+            self.length += 1
+        else:
+            self.storage[self.pointer] = item
+            self.pointer += 1
+            if self.pointer == self.capacity:
+                self.pointer = 0
 
     def get(self):
-        pass
+        return self.storage
