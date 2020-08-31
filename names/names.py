eb38bf793203a27ffe5d1f554fc801d3b2f3dba1
Your name <a.m.verdine@gmail.com>
2020-08-30 20:18:49 -0400

    optimized code for names.py

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from names_bst import BinarySearchTree
 
 start_time = time.time()
 
@@ -12,15 +13,25 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
+tree = BinarySearchTree("please")
+
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+for i in names_1:
+    tree.insert(i)
+
+for j in names_2:
+    if tree.contains(j):
+        duplicates.append(j)
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+
+print(f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print(f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
