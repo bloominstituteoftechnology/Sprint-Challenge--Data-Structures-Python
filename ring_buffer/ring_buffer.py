2c9099f2541f19048cf90060848612c932fec5db
Ariunbold <ariun1118@gmail.com>
2020-08-30 21:03:51 -0400

    MVP done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,33 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
+        self.capacity = capacity
+        self.arr = []
+        self.counter = 0
     def append(self, item):
-        pass
+        if len(self.arr) == self.capacity:
+            self.arr[self.counter] = item
+            """ Above code is same as the commented code """
+            # self.arr.pop(self.counter)
+            # self.arr.insert(self.counter, item)
+            if self.counter + 1 == self.capacity:
+                self.counter = 0
+            else:
+                self.counter += 1
+        else:
+            self.arr.append(item)
+        return item         
 
     def get(self):
-        pass
+        return self.arr
+
+rb = RingBuffer(3)
+
+rb.append(2)
+rb.append(1)
+rb.append(4)
+rb.append(5)
+rb.append(6)
+rb.append(7)
+rb.append(8)
+
+print(f"{rb.get()}")
