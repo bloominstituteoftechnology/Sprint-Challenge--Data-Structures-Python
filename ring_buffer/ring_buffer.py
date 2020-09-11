e599830d638e3a219a711a2749686aafdacfb877
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2020-09-11 09:12:01 -0700

    first tests passing 5/6

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
+
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
-
+    
+    def __init__(self, capacity, storage=None, temp=0):
+        self.capacity = capacity
+        self.storage = []
+        self.temp = temp
+   
     def append(self, item):
-        pass
-
+        self.storage.append(item)
+        if len(self.storage) > self.capacity:
+            self.storage[self.temp] = item
+            self.storage.pop()          
+            self.temp += 1
+        if self.temp > self.capacity:
+            self.temp = 0
+        
+    
     def get(self):
-        pass
+        return self.storage
