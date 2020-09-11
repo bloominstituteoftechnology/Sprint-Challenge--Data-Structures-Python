f1a098ac899270566ecac79db6ade8c19baa6191
Cody Morley <codymorleyinc@gmail.com>
2020-09-11 10:32:20 -0700

    Reversed List
    Finished Ring Buffer bug fixes and implemented list reversal in second problem. 2/3 MVP

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -4,37 +4,32 @@ class RingBuffer:
         self.capacity = capacity
         self.head = 0
         self.tail = 0
+        self.index = 0
 
     def iterate_head(self):
-        if self.head == self.capacity:
+        if self.head >= self.capacity -1:
             self.head = 0
         else:
             self.head += 1
 
     def iterate_tail(self):
-        if self.tail == self.capacity:
+        if self.tail >= self.capacity - 1:
             self.tail = 0
         else:
             self.tail += 1
 
+    def size(self):
+        return len(self.q)
+
     def append(self, item):
-        if len(self.q) == self.capacity:
-            tail_item = self.q[self.tail]
-            self.q.remove(tail_item)
-            self.q.insert(self.tail, item)
+        if self.size() == self.capacity:
+            self.q[self.tail] = item
             self.iterate_tail()
         else:
-            self.q.insert(self.tail, item)
+            self.q.append(item)
             self.iterate_tail()
 
     def get(self):
-        start = self.head
-        self.iterate_head()
-        return_list = list()
-        while self.head != start:
-            item = self.q[self.head]
-            if item:
-                return_list.append(item)
-            self.iterate_head()
-        return return_list
+        return self.q
+            
 
