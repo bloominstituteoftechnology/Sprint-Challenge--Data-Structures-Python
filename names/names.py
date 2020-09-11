da143013318662b5b0e3b929ef002cfc3a1eefe7
Cody Morley <codymorleyinc@gmail.com>
2020-09-11 11:32:07 -0700

    Names
    Solved. MVP

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from name_tree import BSTNode
 
 start_time = time.time()
 
@@ -13,10 +14,20 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
+"""
 for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
             duplicates.append(name_1)
+"""
+root = BSTNode(names_1[0])
+for i in range(1, (len(names_1) - 1), 1):
+    root.insert(names_1[i])
+
+for name in names_2:
+    if root.contains(name):
+        duplicates.append(name)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
