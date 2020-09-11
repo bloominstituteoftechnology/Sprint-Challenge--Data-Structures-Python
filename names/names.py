f8d0e985f1ebea42a338fb138546615938f0a975
Anthony N <foto.arn@gmail.com>
2020-09-11 10:07:17 -0700

    + fixed error in names

--- names/names.py
+++ names/names.py
@@ -11,21 +11,14 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-# Return the list of duplicates in this data structure
-duplicates = []
+duplicates = []  # Return the list of duplicates in this data structure
 bst = BSTNode("")
-
-
-def printEach(node):
-    print(f"{node},")
 
 # Replace the nested for loops below with your improvements
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
-
-
 for name in names_1:
     bst.insert(name)
 
@@ -34,8 +27,9 @@ for name in names_2:
         duplicates.append(name)
 
 end_time = time.time()
-print(f"{len(duplicates)} duplicates:\n\n{bst.for_each(printEach)}\n\n")
-print(f"runtime: {end_time - start_time} seconds")
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"runtime: {end_time - start_time} seconds")
+
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
