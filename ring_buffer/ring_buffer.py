991e714a55d9e7b171fc5b777852584d086e5c17
Alex Thompson <guitarman226@gmail.com>
2020-09-26 14:16:10 -0500

    Ring buffer
    Methods

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,6 +6,21 @@ class RingBuffer:
         self.tail = 0
         self.index = 0
         
+    def iterate_head(self):
+        if self.head >= self.capacity -1:
+            self.head = 0
+        else:
+            self.head += 1
+            
+    def iterate_tail(self):
+        if self.tail >= self.capacity -1:
+            self.tail = 0
+        else:
+            self.tail += 1
+            
+    def size(self):
+        return len(self.q)
+        
     def append(self, item):
         pass
 
