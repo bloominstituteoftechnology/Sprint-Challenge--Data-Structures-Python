f0a09280b51650374a9bf8492d7d99ee8b0f5a07
Claudia Maciel <clc_80@yahoo.com>
2020-09-25 18:45:01 -0700

    Completed Ring_Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,32 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.structure = []
+        self.structure_index = 0
 
     def append(self, item):
-        pass
+        # fill array once
+        if len(self.structure) < self.capacity:
+            self.structure.append(item)
+      
+        else:
+            # make sure the index isn't the same size of capacity 
+            if self.structure_index < self.capacity:
+                #remove the previous item
+                self.structure.pop(self.structure_index)
+                # add to the structure
+                self.structure.insert(self.structure_index, item)
+                #increase index by one
+                self.structure_index += 1
+            else: 
+                #reset to zero
+                self.structure_index = 0
+                # remove from index
+                self.structure.pop(self.structure_index)
+                # add to the structure
+                self.structure.insert(self.structure_index, item)
+                #increase index by one
+                self.structure_index += 1
 
     def get(self):
-        pass
+        return self.structure
