0ae4f3d2f7e12065a92701390560d5dcb6d9ee15
Chad Parker <chad@chadparker.com>
2020-09-25 19:15:13 -0700

    Implement ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,25 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.list = []
+        self.oldest_index = 0
 
     def append(self, item):
-        pass
+        """
+        Adds the given item to the buffer
+        """
+        if len(self.list) < self.capacity:
+            self.list.append(item)
+        else:
+            self.list[self.oldest_index] = item
+            if self.oldest_index < len(self.list) - 1:
+                self.oldest_index += 1
+            else:
+                self.oldest_index = 0
 
     def get(self):
-        pass
+        """
+        Returns all of the elements in the buffer, as a list, in their given order.
+        Does not return any None values in the list, even if they are present in the ring buffer.
+        """
+        return [item for item in self.list if item]
