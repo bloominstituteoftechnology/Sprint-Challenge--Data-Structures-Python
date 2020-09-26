8d7b86d3727485dae110c166f469d19b0154546e
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2020-09-26 13:43:19 -0400

    feat(ring_buffer.py): Completed append and get functions; passes tests

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,10 +2,18 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.current_index = 0
-        self.value = capacity
+        self.storage = []
 
     def append(self, item):
-        pass
+        """  
+            When buffer is full, place the value of `item` at the current index;
+            otherwise, append the item to the end of the buffer
+        """
+        if len(self.storage) == self.capacity:
+            self.storage[self.current_index] = item
+            self.current_index = (self.current_index + 1) % self.capacity
+        else:
+            self.storage.append(item)
 
     def get(self):
-        pass
+        return self.storage
