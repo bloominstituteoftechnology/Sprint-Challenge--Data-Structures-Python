6c39a9db599d29d743cb0f56ab8df6d46c0340c0
Chad Parker <chad@chadparker.com>
2020-09-25 19:56:33 -0700

    Use a binary search tree to hold names_1, going from 6 sec to 0.1 sec

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_search_tree import BSTNode
 
 start_time = time.time()
 
@@ -13,10 +14,18 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+bst = BSTNode('')
+for name1 in names_1:
+    bst.insert(name1)
+
+for name2 in names_2:
+    if bst.contains(name2):
+        duplicates.append(name2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
