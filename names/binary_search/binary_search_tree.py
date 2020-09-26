4263a9206ea6034a962ab1c45a81e07cc95eb1da
Christian Lorenzo <clbmiami2004@gmail.com>
2020-09-26 12:32:52 -0700

    Update binary_search_tree.py
    Finished binary_tree_search document.

--- names/binary_search/binary_search_tree.py
+++ names/binary_search/binary_search_tree.py
@@ -28,3 +28,26 @@ class BSTNode:
         self.left = None
         self.right = None
         
+    #Insert the given value into the tree
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left = insert(value)
+        elif value >= self.value:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+                
+    
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif target < self.value:
+            return self.left.contains(target) if self.left else False
+        else:
+            return self.right.contains(target) if self.right else False
