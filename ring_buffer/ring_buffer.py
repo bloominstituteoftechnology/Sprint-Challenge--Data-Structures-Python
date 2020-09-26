7c9b5a044e2553e652056bae15504ddf04902f23
Josh schaben <jschaben42@gmail.com>
2020-09-25 20:39:32 -0500

    ring buffer passed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.arr = []
+        self.counter = 0
 
     def append(self, item):
-        pass
+        if len(self.arr) is self.capacity:
+            self.arr[self.counter] = item
+
+            if self.counter + 1 is self.capacity:
+                self.counter = 0
+
+            else:
+                self.counter +=1
+
+        else:
+            self.arr.append(item)
+            return item
 
     def get(self):
-        pass
+        return self.arr
