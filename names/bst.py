83a6685c7618e3b5d14c97b617ffe9b1bb02922b
Anthony <chitowncoder@gmail.com>
2020-09-27 22:13:00 -0500

    name.py is completed and speed is amazing, on to reverse

---
+++ names/bst.py
@@ -0,0 +1,38 @@
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    ########## RETURNING INSERT VALUES ##########
+    
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+        elif value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+
+########## RETURNING TRUE VALUE ##########
+########## IF DOES NOT ITS FALSE #########
+
+
+    def contains(self, target):
+        if self.value == target:
+            return True
+        if target < self.value:
+            if not self.left:
+                return False
+            else:
+                return self.left.contains(target)
+        else:
+            if not self.right:
+                return False
+            else:
+                return self.right.contains(target)
