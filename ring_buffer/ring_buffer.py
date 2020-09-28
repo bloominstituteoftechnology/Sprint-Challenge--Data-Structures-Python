04daee4d897838655fe6f47ce6b1e384b993cd49
Anthony <chitowncoder@gmail.com>
2020-09-27 21:01:19 -0500

    Ring Buffer is Complete will work on Names next

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.buffer = []  # SIZE/COUNT ##########
+        self.current = 0  # EMPTY ARRAY - OBJECT ##########
 
     def append(self, item):
-        pass
+        ########## APPENDING AN ELEMENT ##########
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(item)
+        elif len(self.buffer) == self.capacity:
+            self.buffer[self.current] = item
+            # OVERWRITE OLDEST ELEMENT ##########
+            self.current = (self.current + 1) % self.capacity
 
     def get(self):
-        pass
+        ########## RETURNING A LIST OF ELEMENTS [OLDEST-NEWEST] ##########
+        if self.buffer is not None:
+            # SLICED WITH THE COLON ON THE END IS FOR TESTING AND IF IT IS IN THE FRONT IT WILL FAIL ##########
+            return self.buffer[:self.current]+self.buffer[self.current:]
