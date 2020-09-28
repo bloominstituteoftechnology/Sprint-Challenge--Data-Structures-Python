78218c02846e6aadfd8bf900928cc341c706a3ad
Anthony <chitowncoder@gmail.com>
2020-09-27 22:35:12 -0500

    finished reverse.py and have Met MVP

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -1,4 +1,5 @@
 class Node:
+    ########## INITIALIZE CONSTRUCTOR ##########
     def __init__(self, value=None, next_node=None):
         self.value = value
         self.next_node = next_node
@@ -13,9 +14,12 @@ class Node:
         self.next_node = new_next
 
 class LinkedList:
+    ########## INITIALIZING THE HEAD ##########
     def __init__(self):
         self.head = None
-
+        
+        
+    ########## NODE INSERTED IN FRONT ##########
     def add_to_head(self, value):
         node = Node(value)
 
@@ -24,19 +28,49 @@ class LinkedList:
 
         self.head = node
 
+
+
+        ########## TRUE OR FALSE ##########
     def contains(self, value):
         if not self.head:
             return False
-
+        
+        
+        ########## LOOPING THROUGH NODES ##########
         current = self.head
 
         while current:
+            ########## MAKING SURE THIS IS THE NODE WE LOOKED FOR ##########
             if current.get_value() == value:
                 return True
 
             current = current.get_next()
 
         return False
+    
+    
+    ########## 3 POINTERS == PREV/NULL == HEAD & NEXT ##########
+    def reverse_list(self, x=None, y=None): 
+        if self.head == None:
+            return None
+        current = self.head
+        prev = None
+        
+        while current != None:
+            next = current.next_node
+            
+            current.next_node = prev
+            
+            prev = current
+            current = next
+            
+        self.head = prev
+        
+        
+    ########## PRINTING LINKEDLIST ##########
+    def print_list(self):
+        current = self.head
+        while(current):
+            print(current.value)
+            current = current.next_node
 
-    def reverse_list(self, node, prev):
-        pass
