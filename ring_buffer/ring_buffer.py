cc2284df237059774d2b6913d791f2aea77c6bc2
Cai Nowicki <cai.nowicki@gmail.com>
2020-09-27 21:19:07 -0400

    initial attempts at ring buffer class

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,24 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.full = False
+        self.storage = []
+        self.counter = 0
+
+    def is_full(self, capacity):
+        if len(self.storage) > capacity:
+            self.full = True
 
     def append(self, item):
-        pass
+        if not self.full:
+            self.storage.append(item)
+        else:
+            self.storage[self.counter]
+            self.counter = (self.counter + 1) % self.capacity
+        self.is_full(self.capacity)
 
     def get(self):
-        pass
+        if self.full:
+            return self.storage
+        else:
+            return [x for x in self.storage if x is not None]
