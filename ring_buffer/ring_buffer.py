cfe4e14e3adaae0f0552ec168a9eac342540fb17
Abdulhamid Azizy <55733008+HamidAzizy@users.noreply.github.com>
2020-09-27 19:41:33 -0700

    Project done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,45 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        self.index =0
+        self.size =0
 
     def append(self, item):
-        pass
+        # self.data.append(item)
+        if self.size >= self.capacity:
+        	self.data[self.index] = item
+            #  self.size += 1
+        else:    
+            self.data.append(item)
+            self.size += 1
+
+        if self.index < (self.capacity -1):
+            self.index +=1
+            print('append', self.data)
+        else:
+            self.index = 0
 
     def get(self):
-        pass
+        while self.data:
+        	print('I got', self.data)
+        	return self.data
+
+buffer = RingBuffer(3)
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+buffer.append('d')
+buffer.append('e')
+buffer.append('f')
+buffer.append('g')
+buffer.append('h')
+buffer.append('i')
+buffer.append('j')
+buffer.append('k')
+buffer.append('l')
+buffer.append('m')
+
+
+buffer.get()
