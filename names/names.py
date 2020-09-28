b983922bddeb8be0eab497ede63a18ac45b021fd
Kelson Hartle <kelson99hartle@icloud.com>
2020-09-27 20:17:18 -0600

    MVP

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from binary_tree import BSTNode
 
 start_time = time.time()
 
@@ -10,13 +11,26 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+#  probably best here to use a binary search tree and loop through the lists
+# of names and then 
+
+binary_tree = BSTNode('tree')
+
+for name in names_1:
+    binary_tree.insert(name)
+
+for name in names_2:
+    if binary_tree.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -26,3 +40,5 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# duplicates = set(names_1).intersection(names_2)
--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,34 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = []
+
+    oldest = 0
 
     def append(self, item):
-        pass
+        print("------")
+        print(self.storage)
+        print(len(self.storage))
+
+        if len(self.storage) == self.capacity:
+            self.storage.remove(self.storage[self.oldest])
+            self.storage.insert(self.oldest, item)
+            if self.oldest + 1 == 5:
+                self.oldest = 0
+            else:
+                self.oldest += 1
+            print("AFTER")
+            print(self.storage)
+
+        elif len(self.storage) == 0:
+            self.storage.insert(0, item)
+
+        else:
+            self.storage.append(item)
 
     def get(self):
-        pass
+        if len(self.storage) == 0:
+            return []
+
+        else:
+            return self.storage
