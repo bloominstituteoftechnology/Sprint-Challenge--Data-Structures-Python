5a6ba5034f143ae2ad2f9f9d93338d7394c1016e
FabiolaSaga <saga.fabiola@gmail.com>
2020-09-28 09:44:58 -0500

    Finished Reversed

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,4 +39,10 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if node is None:
+            self.head = prev
+            return
+        
+        next_node = node.get_next()
+        node.set_next(prev)
+        self.reverse_list(next_node, node)
