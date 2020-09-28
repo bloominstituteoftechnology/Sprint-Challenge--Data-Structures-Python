f9e8356417dea2172734e6ff04bf4eaaed5b854a
Cristina Altreche <c.altreche@gmail.com>
2020-09-27 22:05:15 -0400

    ring buffer completed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -3,22 +3,41 @@ class RingBuffer:
         # setup max capacity and storage
         self.capacity = capacity
         self.storage = []
-        # setup size to zero
+        # initialize ring buffer size to zero
         self.size = 0
         # instantiate the tail and head to insert and remove oldest and newest
-        self.tail = -1 # to equal newest element
-        self.head = 0 # to equal oldest element
+        self.tail = -1  # to equal newest element
+        self.head = 0  # to equal oldest element
 
+# https://www.oreilly.com/library/view/python-cookbook/0596001673/ch05s19.html
+# class _ _Full:
+#         """ class that implements a full buffer """
+#         def append(self, x):
+#             """ Append an element overwriting the oldest one. """
+#             self.data[self.cur] = x
+#             self.cur = (self.cur+1) % self.max
 
     def append(self, item):
         # first check if the storage is full
-        if self.size == self.capacity
-            # if full enqueue the item to the tail
+        if self.size == self.capacity:
+            # replace index at self.head
+            self.storage[self.head] = item
 
-            # decrease the size 
+            # update head to be oldest item in storage. When this leaves no remainders start back at 0
+            self.head = (self.head + 1) % self.capacity
 
-            # otherwise enqueue the item
+        # otherwise if storage is not full enqueue item
+        else:
+            self.enqueue(item)
 
     def get(self):
         # returns all of the elements in the buffer in a list
         return self.storage
+
+    # added enqueue to insert values at end of RingBuffer.
+
+    def enqueue(self, value):
+        # inserts the value at the end of the storage
+        self.storage.append(value)
+        # increases size by 1
+        self.size += 1
