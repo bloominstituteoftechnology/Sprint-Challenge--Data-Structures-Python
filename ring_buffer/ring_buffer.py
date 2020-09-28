d45041b03e5c9f3fda36b3484ad876106f37c3e7
Cristina Altreche <c.altreche@gmail.com>
2020-09-27 21:25:44 -0400

    initialized size, head, and tail for RingBuffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -3,6 +3,12 @@ class RingBuffer:
         # setup max capacity and storage
         self.capacity = capacity
         self.storage = []
+        # setup size to zero
+        self.size = 0
+        # instantiate the tail and head to insert and remove oldest and newest
+        self.tail = -1 # to equal newest element
+        self.head = 0 # to equal oldest element
+
 
     def append(self, item):
         pass
